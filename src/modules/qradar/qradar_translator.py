980b5b52dd6a6ec16ff2520c1e8941591eec1a87
Danny Elliott <danny.elliott@ibm.com>
2018-05-23 11:22:31 -0300

    main stix_shifter python file and qradar module layout

---
+++ src/modules/qradar/qradar_translator.py
@@ -0,0 +1,9 @@
+# Module to handle conversion
+
+
+def stix_to_aql(pattern: str) -> str:
+    return "Some AQL query"
+
+
+def qradar_to_stix(qradar_query_results):
+    return "QRadar events as STIX observables in JSON format"
---
+++ src/stix_shifter.py
@@ -0,0 +1,9 @@
+# Main module to be called
+
+
+def main():
+    print("Nothing to see here yet")
+
+
+if __name__ == "__main__":
+    main()
