b29c6a6e30cbdf672a73e006ed60419f7ef31e57
Danny Elliott <danny.elliott@ibm.com>
2018-05-28 14:53:55 -0300

    move pattern-to-aql mapping to it's own json file

--- src/modules/qradar/qradar_data_mapping.py
+++ src/modules/qradar/qradar_data_mapping.py
@@ -1,28 +1,35 @@
+import json
+
+
+def _fetch_mapping():
+    try:
+        map_file = open(
+            'src/modules/qradar/from_stix_map.json').read()
+        map_data = json.loads(map_file)
+        return map_data
+    except Exception as ex:
+        print('exception in main():', ex)
+        return {}
+
+
+class DataMappingException(Exception):
+    pass
+
+
 class QRadarDataMapper:
 
-    MAPPINGS = {
-        "ipv4-addr": {
-            "fields": {
-                "value": ["destinationip", "sourceip"]
-            }
-        },
-        "ipv6-addr": {
-            "fields": {
-                "value": ["destinationip", "sourceip"]
-            }
-        }
-    }
-
     def map_object(self, stix_object_name):
-        if stix_object_name in self.MAPPINGS and self.MAPPINGS[stix_object_name] != None:
-            return self.MAPPINGS[stix_object_name]
+        self.map_data = _fetch_mapping()
+        if stix_object_name in self.map_data and self.map_data[stix_object_name] != None:
+            return self.map_data[stix_object_name]
         else:
             raise DataMappingException(
                 "Unable to map object `{}` into AQL".format(stix_object_name))
 
     def map_field(self, stix_object_name, stix_property_name):
-        if stix_object_name in self.MAPPINGS and stix_property_name in self.MAPPINGS[stix_object_name]["fields"]:
-            return self.MAPPINGS[stix_object_name]["fields"][stix_property_name]
+        self.map_data = _fetch_mapping()
+        if stix_object_name in self.map_data and stix_property_name in self.map_data[stix_object_name]["fields"]:
+            return self.map_data[stix_object_name]["fields"][stix_property_name]
         else:
             raise DataMappingException("Unable to map property `{}:{}` into AQL".format(
                 stix_object_name, stix_property_name))
