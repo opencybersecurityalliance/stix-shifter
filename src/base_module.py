f13ceb04b6aff233bcf2ed39e3059c2206f4cf3f
Logan Marks <logan.marks@ibm.com>
2018-05-30 17:01:35 -0300

    WIP setup SDK interface

--- src/base_module.py
+++ src/base_module.py
@@ -8,15 +8,9 @@ class TranslationInterface:
     INPUT_DATA_MODELS = ['sco']
 
     def stix_to_datasource_query(self, arg):
-        # if translating STIX pattern to AQL...
-        stix_pattern = arg[2]
-        translator = qradar_translator.QRadarTranslator()
-        aql_query = translator.stix_to_aql(stix_pattern)
-        return aql_query
+        # if translating STIX pattern to a datasource query...
+        raise NotImplementedError
 
     def datasource_to_stix(self, arg):
-        # if translating QRadar events to STIX...
-        translator = qradar_translator.QRadarTranslator()
-        data = json.loads(arg[2])
-        stix_observables = translator.qradar_to_stix(data)
-        return stix_observables
+        # if translating some datasource to STIX results...
+        raise NotImplementedError
