df2fcb44fa1b70e934cfa76df76968ff8058083c
Logan Marks <logan.marks@ibm.com>
2018-05-30 17:01:35 -0300

    WIP setup SDK interface

---
+++ src/modules/qradar/qradar_module.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import json
+from ..qradar import qradar_translator
+from ...base_module import TranslationInterface
+
+
+class Translator(TranslationInterface):
+
+    def stix_to_datasource_query(self, arg):
+        # if translating STIX pattern to AQL...
+        stix_pattern = arg[2]
+        translator = qradar_translator.QRadarTranslator()
+        aql_query = translator.stix_to_aql(stix_pattern)
+        return aql_query
+
+    def datasource_to_stix(self, arg):
+        # if translating QRadar events to STIX...
+        translator = qradar_translator.QRadarTranslator()
+
+        data = json.loads(arg[2])
+
+        stix_observables = translator.qradar_to_stix(data)
+
+        print(stix_observables)
+        return stix_observables
