531b8f4332ea635321fed38eb399e8ea9514d79d
ben.craig@ca.ibm.com <ben.craig@ca.ibm.com>
2018-06-01 12:50:55 -0300

    fix: command line parser.

--- stix_shifter.py
+++ stix_shifter.py
@@ -1,32 +1,80 @@
 # Main module to be called
 from src import base_module
 import sys
+import argparse
 
 
-def main():
+class StixShifter:
+    """
+    StixShifter class - implements the features of qapputils
+    """
 
-    # In the case of converting a stix pattern to datasource query, arguments will take the form of...
-    # <data_source> <input_format> <stix_pattern>
-    # The data_source and input_format will determine what module and method gets called
-    interface = base_module.TranslationInterface()
+    def __init__(self):
+        self.args = []
 
-    input_arguments = sys.argv[1:]
-    data_source_module = input_arguments[0]
-    input_data_model = input_arguments[1]
+    def translateResults(self):
+        """
+        Display the output of all of the data collected from the various services
+            default is table format
+            -j will return in json format
+            -r (with -j) will return the additional raw data collected from the services)
+        :param qapps: collection of QApp objects
+        :type qapps: dict(string,Qapp)
+        :return: exit code to exit with
+        :rtype: integer
+        """
+        print('hi')
 
-    if(input_data_model in interface.INPUT_DATA_MODELS):
-        # Converting STIX pattern to datasource query
-        query = interface.stix_to_datasource_query(input_arguments)
-        print(query)
-        exit()
-    elif(input_data_model == 'qradar_events'):
-        # Converting data from the data source to STIX objects
-        stix_observables = interface.datasource_to_stix(input_arguments)
-        print(stix_observables)
-        exit()
-    else:
-        raise NotImplementedError
+    def translateQuery(self):
+        """
+        Display the output of all of the data collected from the various services
+            default is table format
+            -j will return in json format
+            -r (with -j) will return the additional raw data collected from the services)
+        :param qapps: collection of QApp objects
+        :type qapps: dict(string,Qapp)
+        :return: exit code to exit with
+        :rtype: integer
+        """
+        print('hi')
+
+    def main(self):
+        """
+        Main function that sets up argument parsing then calls the proper command function
+        In the case of converting a stix pattern to datasource query, arguments will take the form of...
+        <data_source> <input_format> <stix_pattern>
+        The data_source and input_format will determine what module and method gets called
+        """
+
+        # Process arguments
+        parser = argparse.ArgumentParser(description='stix_shifter')
+        subparsers = parser.add_subparsers(dest='command', help="sub-command help")
+
+        # translate query command
+        query_parser = subparsers.add_parser('translateQuery', help='Translate an input query to another format')
+        query_parser.add_argument('format_out', help='What format should the query be translated to', choices=['qradar'])
+        query_parser.add_argument('format_in', help='What format is the given query in',  choices=['sco'])
+        query_parser.add_argument('query', help='The query string to be translated', type=str)
+        query_parser.add_argument('-p', '--pretty', action="store_true", help='Optional Argument')
+
+        # translate results command
+        result_parser = subparsers.add_parser('translateResults', help="Translate a result set to another format")
+        result_parser.add_argument('format_out', help='What format should the results be translated to', choices=['stix'])
+        result_parser.add_argument('format_in', help='What format is the given results in',  choices=['qradar'])
+        result_parser.add_argument('results', help='The results to be translated ', type=str)
+        result_parser.add_argument('-p', '--pretty', action="store_true", help='Optional Argument')
+
+        self.args = parser.parse_args()
+
+        if self.args.command is None:
+            parser.print_help(sys.stderr)
+            sys.exit(1)
+
+        methodToCall = getattr(self, "%s" % self.args.command)
+        exit_code = methodToCall()
+        exit(exit_code)
 
 
 if __name__ == "__main__":
-    main()
+    Shifter = StixShifter()
+    Shifter.main()
