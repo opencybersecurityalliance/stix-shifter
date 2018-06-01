5e0ab74d7c826bfa4ce0b346e7f6a6b6ca6a90fb
ben.craig@ca.ibm.com <ben.craig@ca.ibm.com>
2018-06-01 15:19:37 -0300

    feat: add command line parser

--- src/base_module.py
+++ src/base_module.py
@@ -9,7 +9,7 @@ class TranslationInterface:
 
     def stix_to_datasource_query(self, arg):
         # if translating STIX pattern to AQL...
-        stix_pattern = arg[2]
+        stix_pattern = arg;
         translator = qradar_translator.QRadarTranslator()
         aql_query = translator.stix_to_aql(stix_pattern)
         return aql_query
@@ -17,6 +17,6 @@ class TranslationInterface:
     def datasource_to_stix(self, arg):
         # if translating QRadar events to STIX...
         translator = qradar_translator.QRadarTranslator()
-        data = json.loads(arg[2])
+        data = json.loads(arg)
         stix_observables = translator.qradar_to_stix(data)
         return stix_observables
--- stix_shifter.py
+++ stix_shifter.py
@@ -12,31 +12,37 @@ class StixShifter:
     def __init__(self):
         self.args = []
 
-    def translateResults(self):
+    def translateResults(self, format_out, format_in, results):
         """
-        Display the output of all of the data collected from the various services
-            default is table format
-            -j will return in json format
-            -r (with -j) will return the additional raw data collected from the services)
-        :param qapps: collection of QApp objects
-        :type qapps: dict(string,Qapp)
-        :return: exit code to exit with
-        :rtype: integer
+        Translated result sets to a specified format
+        :param format_out: the format to translate the results to
+        :type format_out: enum of format_out
+        :param format_in: the format of the results
+        :type format_in: enum of format_in
+        :param results: the results to translate
+        :type results: str
+        :return: translated results
+        :rtype: str
         """
-        print('hi')
+        interface = base_module.TranslationInterface()
+        stix_observables = interface.datasource_to_stix(results)
+        print(stix_observables)
 
-    def translateQuery(self):
+    def translateQuery(self, format_out, format_in, query):
         """
-        Display the output of all of the data collected from the various services
-            default is table format
-            -j will return in json format
-            -r (with -j) will return the additional raw data collected from the services)
-        :param qapps: collection of QApp objects
-        :type qapps: dict(string,Qapp)
-        :return: exit code to exit with
-        :rtype: integer
+        Translated queries to a specified format
+        :param format_out: the format to translate the query to
+        :type format_out: enum of format_out
+        :param format_in: the format of the results
+        :type format_in: enum of format_in
+        :param query: the query to translate
+        :type query: str
+        :return: translated results
+        :rtype: str
         """
-        print('hi')
+        interface = base_module.TranslationInterface()
+        query = interface.stix_to_datasource_query(query)
+        print(query)
 
     def main(self):
         """
@@ -52,15 +58,17 @@ class StixShifter:
 
         # translate query command
         query_parser = subparsers.add_parser('translateQuery', help='Translate an input query to another format')
-        query_parser.add_argument('format_out', help='What format should the query be translated to', choices=['qradar'])
-        query_parser.add_argument('format_in', help='What format is the given query in',  choices=['sco'])
+        query_parser.add_argument('format_out', help='What format should the query be translated to',
+                                  choices=['qradar'])
+        query_parser.add_argument('format_in', help='What format is the given query in', choices=['sco'])
         query_parser.add_argument('query', help='The query string to be translated', type=str)
         query_parser.add_argument('-p', '--pretty', action="store_true", help='Optional Argument')
 
         # translate results command
         result_parser = subparsers.add_parser('translateResults', help="Translate a result set to another format")
-        result_parser.add_argument('format_out', help='What format should the results be translated to', choices=['stix'])
-        result_parser.add_argument('format_in', help='What format is the given results in',  choices=['qradar'])
+        result_parser.add_argument('format_out', help='What format should the results be translated to',
+                                   choices=['stix'])
+        result_parser.add_argument('format_in', help='What format is the given results in', choices=['qradar'])
         result_parser.add_argument('results', help='The results to be translated ', type=str)
         result_parser.add_argument('-p', '--pretty', action="store_true", help='Optional Argument')
 
@@ -70,9 +78,14 @@ class StixShifter:
             parser.print_help(sys.stderr)
             sys.exit(1)
 
-        methodToCall = getattr(self, "%s" % self.args.command)
-        exit_code = methodToCall()
-        exit(exit_code)
+        command_to_use = self.args.command
+
+        if command_to_use == 'translateQuery':
+            self.translateQuery(self.args.format_out, self.args.format_in, self.args.query)
+        else:
+            self.translateResults(self.args.format_out, self.args.format_in, self.args.results)
+
+        exit(0)
 
 
 if __name__ == "__main__":
