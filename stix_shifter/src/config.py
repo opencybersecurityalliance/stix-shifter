6dbb3731969e8c1f5a214315e1cb61c544cc12bc
Bhavesh Kumar <bhavesh.kumar@ibm.com>
2018-10-22 13:51:02 +0100

    removed global config and kept it at the module level

--- stix_shifter/src/config.py
+++
@@ -1,9 +0,0 @@
-class SplunkConfig:
-    # Configuration related to Splunk module
-    DEFAULT_LIMIT = 10000
-    DEFAULT_TIMERANGE = 5
-
-
-class QRadarConfig:
-    # Configuration related to QRadar module
-    pass
