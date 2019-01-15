ebd97b59301b537b5ab1c12779e72690cdb13194
Danny Elliott <danny.elliott@ibm.com>
2019-01-15 11:43:18 -0400

    src_ref and dst_ref pattern support for the dummy module

--- stix_shifter/stix_translation/src/modules/dummy/query_constructor.py
+++ stix_shifter/stix_translation/src/modules/dummy/query_constructor.py
@@ -2,8 +2,16 @@ from stix_shifter.stix_translation.src.patterns.pattern_objects import Observati
     ComparisonExpressionOperators, ComparisonComparators, Pattern, \
     CombinedComparisonExpression, CombinedObservationExpression, ObservationOperators
 from stix_shifter.stix_translation.src.transformers import TimestampToMilliseconds
+from stix_shifter.stix_translation.src.json_to_stix import observable
 import logging
 import re
+
+# Source and destination reference mapping for ip and mac addresses.
+# Change the keys to match the data source fields. The value array indicates the possible data type that can come into from field.
+REFERENCE_DATA_TYPES = {"SourceIpV4": ["ipv4", "ipv4_cidr"],
+                        "SourceIpV6": ["ipv6"],
+                        "DestinationIpV4": ["ipv4", "ipv4_cidr"],
+                        "DestinationIpV6": ["ipv6"]}
 
 logger = logging.getLogger(__name__)
 
@@ -72,6 +80,48 @@ class QueryStringPatternTranslator:
     def _negate_comparison(comparison_string):
         return "NOT({})".format(comparison_string)
 
+    @staticmethod
+    def _check_value_type(value):
+        value = str(value)
+        for key, pattern in observable.REGEX.items():
+            if key != 'date' and bool(re.search(pattern, value)):
+                return key
+        return None
+
+    @staticmethod
+    def _parse_reference(self, stix_field, value_type, mapped_field, value, comparator):
+        if value_type not in REFERENCE_DATA_TYPES["{}".format(mapped_field)]:
+            return None
+        else:
+            return "{mapped_field} {comparator} {value}".format(
+                mapped_field=mapped_field, comparator=comparator, value=value)
+
+    @staticmethod
+    def _parse_mapped_fields(self, expression, value, comparator, stix_field, mapped_fields_array):
+        comparison_string = ""
+        is_reference_value = self._is_reference_value(stix_field)
+        # Need to use expression.value to match against regex since the passed-in value has already been formated.
+        value_type = self._check_value_type(expression.value) if is_reference_value else None
+        mapped_fields_count = 1 if is_reference_value else len(mapped_fields_array)
+
+        for mapped_field in mapped_fields_array:
+            if is_reference_value:
+                parsed_reference = self._parse_reference(self, stix_field, value_type, mapped_field, value, comparator)
+                if not parsed_reference:
+                    continue
+                comparison_string += parsed_reference
+            else:
+                comparison_string += "{mapped_field} {comparator} {value}".format(mapped_field=mapped_field, comparator=comparator, value=value)
+
+            if (mapped_fields_count > 1):
+                comparison_string += " OR "
+                mapped_fields_count -= 1
+        return comparison_string
+
+    @staticmethod
+    def _is_reference_value(stix_field):
+        return stix_field == 'src_ref.value' or stix_field == 'dst_ref.value'
+
     def _parse_expression(self, expression, qualifier=None) -> str:
         if isinstance(expression, ComparisonExpression):  # Base Case
             # Resolve STIX Object Path to a field in the target Data Model
@@ -100,24 +150,8 @@ class QueryStringPatternTranslator:
             else:
                 value = self._escape_value(expression.value)
 
-            comparison_string = ""
-            mapped_fields_count = len(mapped_fields_array)
-            for mapped_field in mapped_fields_array:
-                # TODO: REMOVE THESE SPECIAL CASES
-                # if its a set operator() query construction will be different.
-                if expression.comparator == ComparisonComparators.IsSubSet:
-                    comparison_string += comparator + "(" + "'" + value + "'," + mapped_field + ")"
-                else:
-                    comparison_string += "{mapped_field} {comparator} {value}".format(
-                        mapped_field=mapped_field, comparator=comparator, value=value)
-
-                # Separate each conditional statement that maps to the same STIX attribute.
-                # Use whatever syntax/operator is required by the data source ('OR', ',', ' ', etc.)
-                if (mapped_fields_count > 1):
-                    comparison_string += " OR "
-                    mapped_fields_count -= 1
-
-            if(len(mapped_fields_array) > 1):
+            comparison_string = self._parse_mapped_fields(self, expression, value, comparator, stix_field, mapped_fields_array)
+            if(len(mapped_fields_array) > 1 and not self._is_reference_value(stix_field)):
                 # More than one data source field maps to the STIX attribute, so group comparisons together.
                 grouped_comparison_string = "(" + comparison_string + ")"
                 comparison_string = grouped_comparison_string
