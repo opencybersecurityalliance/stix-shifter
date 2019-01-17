99b4927563331c8738a8c916ddd955ab8c35d397
Danny Elliott <danny.elliott@ibm.com>
2019-01-17 11:24:17 -0400

    add error handling to async dummy connector

--- stix_shifter/stix_transmission/src/modules/async_dummy/async_dummy_connector.py
+++ stix_shifter/stix_transmission/src/modules/async_dummy/async_dummy_connector.py
@@ -27,12 +27,20 @@ class Connector(BaseConnector):
         self.status_connector = self
 
     def ping(self):
-        response = self.api_client.ping_data_source()
-        return response
+        try:
+            response = self.api_client.ping_data_source()
+            return response
+        except Exception as err:
+            print('error when pinging datasource {}:'.format(err))
+            raise
 
     def create_query_connection(self, query):
-        response = self.api_client.create_search(query)
-        return response
+        try:
+            response = self.api_client.create_search(query)
+            return response
+        except Exception as err:
+            print('error when creating search: {}'.format(err))
+            raise
 
     # Map data source status to connector status
     def __getStatus(self, status):
@@ -47,41 +55,53 @@ class Connector(BaseConnector):
         return switcher.get(status).value
 
     def create_status_connection(self, search_id):
-        response = self.api_client.get_search_status(search_id)
-        # Based on the response
-        # return_obj['success'] = True or False
-        # return_obj['status'] = One of the statuses as defined in the Status class:
-        # Status.RUNNING, Status.COMPLETED, Status.CANCELED, Status.ERROR
-        # return_obj['progress'] = Some progress code if returned from the API
-        # Construct a response object
-        response_code = response["code"]
-        return_obj = dict()
+        try:
+            response = self.api_client.get_search_status(search_id)
+            # Based on the response
+            # return_obj['success'] = True or False
+            # return_obj['status'] = One of the statuses as defined in the Status class:
+            # Status.RUNNING, Status.COMPLETED, Status.CANCELED, Status.ERROR
+            # return_obj['progress'] = Some progress code if returned from the API
+            # Construct a response object
+            response_code = response["code"]
+            return_obj = dict()
 
-        if response_code == 200:
-            return_obj['success'] = True
-            return_obj['status'] = self.__getStatus(response["status"])
-        else:
-            return_obj['success'] = False
-            return_obj['error'] = response['message']
-        return return_obj
+            if response_code == 200:
+                return_obj['success'] = True
+                return_obj['status'] = self.__getStatus(response["status"])
+            else:
+                return_obj['success'] = False
+                return_obj['error'] = response['message']
+            return return_obj
+        except Exception as err:
+            print('error when getting search status: {}'.format(err))
+            raise
 
     def create_results_connection(self, search_id, offset, length):
-        min_range = offset
-        max_range = offset + length
-        # Grab the response, extract the response code, and convert it to readable json
-        response = self.api_client.get_search_results(search_id, min_range, max_range)
-        response_code = response["code"]
+        try:
+            min_range = offset
+            max_range = offset + length
+            # Grab the response, extract the response code, and convert it to readable json
+            response = self.api_client.get_search_results(search_id, min_range, max_range)
+            response_code = response["code"]
 
-        # Construct a response object
-        return_obj = dict()
-        if response_code == 200:
-            return_obj['success'] = True
-            return_obj['data'] = response['data']
-        else:
-            return_obj['success'] = False
-            return_obj['error'] = response['message']
-        return return_obj
+            # Construct a response object
+            return_obj = dict()
+            if response_code == 200:
+                return_obj['success'] = True
+                return_obj['data'] = response['data']
+            else:
+                return_obj['success'] = False
+                return_obj['error'] = response['message']
+            return return_obj
+        except Exception as err:
+            print('error when getting search results: {}'.format(err))
+            raise
 
     def delete_query_connection(self, search_id):
-        response = self.api_client.delete_search(search_id)
-        return response
+        try:
+            response = self.api_client.delete_search(search_id)
+            return response
+        except Exception as err:
+            print('error when deleting search {}:'.format(err))
+            raise
