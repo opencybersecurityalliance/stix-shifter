9721ae776f9b2c876a7de6d845d55ee7378a0836
jiss <Jiss.James@ibm.com>
2019-01-17 16:14:02 +0000

    change in regex

--- stix_shifter/stix_translation/src/modules/splunk/splunk_utils.py
+++ stix_shifter/stix_translation/src/modules/splunk/splunk_utils.py
@@ -13,7 +13,7 @@ def hash_type_lookup(obj, ds_key, mapped_stix_attribute, options):
         if re.compile("^[a-f0-9]{32}$").match(obj["file_hash"]) is not None:
             file_hash_map = file_hash_map.format("MD5")
             return file_hash_map
-        elif re.compile("\b[0-9a-f]{5,40}\b").match(obj["file_hash"]) is not None:
+        elif re.compile(r'\b[0-9a-f]{40}\b').match(obj["file_hash"]) is not None:
             file_hash_map = file_hash_map.format("SHA-1")
             return file_hash_map
         elif re.compile("[A-Fa-f0-9]{64}").match(obj["file_hash"]) is not None:
