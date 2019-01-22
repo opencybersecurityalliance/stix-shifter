7d7eea8d700601c7d978a2bdde4e620a22eee5a5
Danny Elliott <danny.elliott@gmail.com>
2019-01-22 16:58:53 -0400

    Update dummy module (#96)

--- tests/stix_transmission/test_async_dummy.py
+++ tests/stix_transmission/test_async_dummy.py
@@ -10,14 +10,12 @@ class TestAsyncDummyConnection(unittest.TestCase, object):
             "port": "8080",
             "path": "/"
         }
-        query_interface = async_dummy_connector.Connector(connection, None)
-
+        interface = async_dummy_connector.Connector(connection, None)
         query = "placeholder query text"
+        query_response = interface.create_query_connection(query)
 
-        query_response = query_interface.create_query_connection(query)
-        query_id = query_response['query_id']
-
-        assert query_id == "uuid_1234567890"
+        assert query_response['query_id'] == "uuid_1234567890"
+        assert query_response['code'] == 200
 
     def test_dummy_async_status(self):
         connection = {
@@ -25,68 +23,29 @@ class TestAsyncDummyConnection(unittest.TestCase, object):
             "port": "8080",
             "path": "/"
         }
-        status_interface = async_dummy_connector.Connector(connection, None)
-
+        interface = async_dummy_connector.Connector(connection, None)
         query_id = "uuid_1234567890"
+        status_response = interface.create_status_connection(query_id)
 
-        status_response = status_interface.create_status_connection(query_id)
+        success = status_response["success"]
+        assert success == True
         status = status_response["status"]
         assert status == Status.COMPLETED.value
 
-    def test_dummy_async_results_error(self):
+    def test_dummy_async_results(self):
         connection = {
             "host": "hostbla",
             "port": "8080",
             "path": "/"
         }
-        results_interface = async_dummy_connector.Connector(connection, None)
-
-        query_id = "uuid_should_error"
-        offset = 0
-        length = 1
-        results_response = results_interface.create_results_connection(query_id, offset, length)
-
-        success = results_response["success"]
-        assert success is not True
-        query_results = results_response["error"]
-        assert query_results == "Error: query results not found"
-
-    def test_dummy_async_results_running(self):
-        connection = {
-            "host": "hostbla",
-            "port": "8080",
-            "path": "/"
-        }
-        results_interface = async_dummy_connector.Connector(connection, None)
-
-        query_id = "uuid_not_done"
-        offset = 0
-        length = 1
-        results_response = results_interface.create_results_connection(query_id, offset, length)
-
-        query_results = results_response["error"]
-        success = results_response["success"]
-        assert success is not True
-        assert query_results == "Query is not finished processing"
-
-    def test_dummy_async_results_success(self):
-        connection = {
-            "host": "hostbla",
-            "port": "8080",
-            "path": "/"
-        }
-        results_interface = async_dummy_connector.Connector(connection, None)
-
+        interface = async_dummy_connector.Connector(connection, None)
         query_id = "uuid_1234567890"
-        offset = 0
-        length = 1
-
-        results_response = results_interface.create_results_connection(query_id, offset, length)
-        query_results = results_response["data"]
+        results_response = interface.create_results_connection(query_id, 1, 1)
 
         success = results_response["success"]
-        assert success
-        assert query_results
+        assert success == True
+        data = results_response["data"]
+        assert data == "Results for search"
 
     def test_is_async(self):
         connection = {
