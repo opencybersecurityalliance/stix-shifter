057bb432b635fa405bebc0257bb947283b914ca1
Collin Richards <collin.richards@ibm.com>
2019-02-13 15:56:15 -0500

    fix qradar re-reading mapping for query translate

--- stix_shifter/stix_translation/src/modules/qradar/qradar_data_mapping.py
+++ stix_shifter/stix_translation/src/modules/qradar/qradar_data_mapping.py
@@ -22,9 +22,9 @@ class QRadarDataMapper:
     def __init__(self, options):
         self.mapping_json = options['mapping'] if 'mapping' in options else {}
         self.select_fields_json = options['select_fields'] if 'select_fields' in options else {}
+        self.map_data = self.mapping_json or _fetch_mapping()
 
     def map_field(self, stix_object_name, stix_property_name):
-        self.map_data = self.mapping_json or _fetch_mapping()
         if stix_object_name in self.map_data and stix_property_name in self.map_data[stix_object_name]["fields"]:
             return self.map_data[stix_object_name]["fields"][stix_property_name]
         else:
