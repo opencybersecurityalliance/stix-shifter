4162ce144ff27c5768aaf5ba332ba2e569a8df72
Md Azam <mdazam@mdazam-ibm-com>
2019-06-13 10:43:33 -0300

    Fix for stix2matcher module not found issue

--- stix_shifter/stix_translation/src/modules/bundle/bundle_translator.py
+++ stix_shifter/stix_translation/src/modules/bundle/bundle_translator.py
@@ -5,13 +5,15 @@ import json, requests
 class Translator(BaseTranslator):
     def transform_query(self, data, options, mapping=None):
         #Data is a STIX pattern and we don't want to touch it
-        return json.dumps(data)
+        return data
 
     def translate_results(self, data_source, data, options, mapping=None):
         #Data is already STIX and we don't want to touch it
-        for obs in data:
+        bundle_data = json.loads(data)
+        data_source = json.loads(data_source)
+        for obs in bundle_data:
             obs["created_by_ref"] = data_source['id']
-        return json.dumps(data)
+        return json.dumps(data, indent=4, sort_keys=False)
 
     def __init__(self):
         self.result_translator = self
