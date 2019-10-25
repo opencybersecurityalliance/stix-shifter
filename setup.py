23d5e80d8fc05d2715ee12ab1ecf4f276ebf85c1
Md Azam <mdazam@ca.ibm.com>
2019-10-25 13:50:50 -0300

    Fix license classifier in setup.py

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
         #     'Topic :: Software Development :: Build Tools',
 
         #     # Pick your license as you wish
-         'License :: OSI Approved :: Apache License :: 2.0',
+         'License :: OSI Approved :: Apache Software License',
 
         #     # Specify the Python versions you support here. In particular, ensure
         #     # that you indicate whether you support Python 2, Python 3 or both.
