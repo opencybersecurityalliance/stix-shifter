ad680f0126e594702ebfe996ed03078707d218d0
saksaini <sakshi17092@iiitd.ac.in>
2019-11-03 19:14:03 +0530

    midified sec_adv_translator.py

--- stix_shifter/stix_translation/src/modules/security_advisor/security_advisor_translator.py
+++ stix_shifter/stix_translation/src/modules/security_advisor/security_advisor_translator.py
@@ -1,30 +1,6 @@
-# from ..base.base_translator import BaseTranslator
 from .security_advisor_query_translator import SecurityAdvisorQueryTranslator
-# # from .security_advisor_result_translator import SecurityAdvisorResultTranslator
 
-# from ...json_to_stix.json_to_stix import JSONToStix
-# from .security_advisor_utils import hash_type_lookup
-
-# from os import path
-
-
-# class Translator(BaseTranslator):
-
-#     def __init__(self):
-
-#         basepath = path.dirname(__file__)
-#         filepath = path.abspath(
-#             path.join(basepath, "json", "to_stix_map.json"))
-
-#         self.mapping_filepath = filepath
-
-#         self.result_translator = JSONToStix(filepath, hash_type_lookup)
-#         self.query_translator = SecurityAdvisorQueryTranslator()
-
-
-# ..base.base_translator import BaseTranslator
 from ..base.base_translator import BaseTranslator
-# from .stix_to_query import StixToQuery
 from ...json_to_stix.json_to_stix import JSONToStix
 from .security_advisor_utils import hash_type_lookup
 
