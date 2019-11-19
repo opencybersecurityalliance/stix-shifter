b538c0649cec988893080924d373ef4fd9b1eb0e
SakshiSaini17092 <sakshi17092@iiitd.ac.in>
2019-11-19 14:47:01 +0530

    review changes

--- stix_shifter/stix_transmission/src/modules/security_advisor/utils/sa_findings_api.py
+++ stix_shifter/stix_transmission/src/modules/security_advisor/utils/sa_findings_api.py
@@ -1,66 +1,44 @@
-import os
 import requests
 
+def apiCall( header , url  ) :
+    try :
+        response = requests.get(url,headers= header)
+        if( response.status_code == 200 ):
+            response = response.json()
+            return(response)
+        else :
+            raise Exception("Status Code in getting " + url +  response.status_code)
+    except Exception as e:
+        raise Exception("Exception in getting response of " + url + str(e) )
 
-def getOccurnece(accountID , accessToken , providerID ,host ) :
+def get_all_occurences(accountID , accessToken, host  ):
 
-    url = host + accountID +"/providers/" + providerID + "/occurrences"
     header = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization' : 'Bearer '  + accessToken,
     }
 
-    try :
-        r = requests.get(url,headers= header)
-        if( r.status_code == 200 ):
-            res = r.json()
-            return(res["occurrences"])
-
-        else :
-            print("status -" + str(r.status_code))
-            print("Status not 200", r.json() )
-    except Exception as e :
-        print("some error occured getting Occurence!!", str(e))
-
-
-def getProviders(accountID , accessToken , host):
-
     url = host  + accountID + "/providers"
-    header = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization' : 'Bearer '  + accessToken ,
-    }
 
-    try :
-        r = requests.get(url,headers= header)
-        if( r.status_code == 200 ):
-            res = r.json()
-            return(res)
+    all_providers  = apiCall( header , url )
+    all_providers = all_providers["providers"]
 
-        else :
-            print("status -" + str(r.status_code))
-            print("Status is not 200!", r.json() )
-    except Exception as e :
-        print("some error occured getting all Providers!!", str(e))
+    list_provider_ids = []
+    for i in range(0, len(all_providers)):
+        id  = all_providers[i]["id"]
+        list_provider_ids.append(id)
 
-def get_all_occurences(accountID , accessToken, host  ):
+    all_occurences = []
 
-    providers  = getProviders(accountID, accessToken , host)
-    providers = providers["providers"]
+    for provider in list_provider_ids:
 
-    list1 = []
-    for i in range(0, len(providers)):
-        id  = providers[i]["id"]
-        list1.append(id)
+        url = host + accountID +"/providers/" + provider + "/occurrences"
+        occurences = apiCall(header, url)
+        occurences = occurences["occurrences"]
 
-    all_occ = []
+        for occurence in occurences:
+            all_occurences.append(occurence)
 
-    for provider in list1:
-        occ = getOccurnece( accountID, accessToken , provider, host )
-        for elem in occ:
-            all_occ.append(elem)
-
-    return all_occ
+    return all_occurences
 
