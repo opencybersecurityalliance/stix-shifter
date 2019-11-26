78fb52ecf782f55d7fd282a1194ee14c6741863e
Danny Elliott <danny.elliott@ibm.com>
2019-11-26 18:37:34 -0400

    change where auth token is created

--- stix_shifter/stix_transmission/src/modules/security_advisor/security_advisor_connector.py
+++ stix_shifter/stix_transmission/src/modules/security_advisor/security_advisor_connector.py
@@ -4,16 +4,12 @@ from .security_advisor_query_connector import SecurityAdvisorQueryConnector
 from .security_advisor_status_connector import SecurityAdvisorStatusConnector
 from .security_advisor_results_connector import SecurityAdvisorResultsConnector
 from .security_advisor_delete_connector import SecurityAdvisorDeleteConnector
-from .security_advisor_auth import SecurityAdvisorAuth
 
 
 class Connector(BaseConnector):
     def __init__(self, connection, configuration):
         auth = configuration.get("auth")
-        api_key = auth.get("apiKey")
         host = connection.get("host")
-        auth_token = SecurityAdvisorAuth(api_key)
-        auth["authToken"] = auth_token
 
         self.query_connector = SecurityAdvisorQueryConnector(host, auth)
         self.status_connector = SecurityAdvisorStatusConnector(host, auth)
