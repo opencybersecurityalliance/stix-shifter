6861af46c83e69b0fd228e2c7cc7e2793303e826
subroto@us.ibm.com <subroto@us.ibm.com>
2019-12-02 19:45:37 -0500

    reverted back to json_to_stix_translator.py to old code.

--- stix_shifter/stix_translation/src/json_to_stix/json_to_stix_translator.py
+++ stix_shifter/stix_translation/src/json_to_stix/json_to_stix_translator.py
@@ -3,7 +3,6 @@ import uuid
 
 from . import observable
 from stix2validator import validate_instance, print_results
-from datetime import datetime
 
 
 # convert JSON data to STIX object using map_data and transformers
@@ -18,8 +17,7 @@ def convert_to_stix(data_source, map_data, data, transformers, options, callback
     identity_id = data_source['id']
     bundle['objects'] += [data_source]
 
-    ds2stix = DataSourceObjToStixObj(
-        identity_id, map_data, transformers, options, callback)
+    ds2stix = DataSourceObjToStixObj(identity_id, map_data, transformers, options, callback)
 
     # map data list to list of transformed objects
     results = list(map(ds2stix.transform, data))
@@ -48,6 +46,7 @@ class DataSourceObjToStixObj:
     def _get_value(obj, ds_key, transformer):
         """
         Get value from source object, transforming if specified
+
         :param obj: the input object we are translating to STIX
         :param ds_key: the property from the input object
         :param transformer: the transform to apply to the property value (can be None)
@@ -65,6 +64,7 @@ class DataSourceObjToStixObj:
     def _add_property(obj, key, stix_value, group=False):
         """
         Add stix_value to dictionary based on the input key, the key can be '.'-separated path to inner object
+
         :param obj: the dictionary we are adding our key to
         :param key: the key to add
         :param stix_value: the STIX value translated from the input object
@@ -78,17 +78,18 @@ class DataSourceObjToStixObj:
                 child_obj[prop] = {}
             child_obj = child_obj[prop]
 
+
         if split_key[-1] not in child_obj.keys():
             child_obj[split_key[-1]] = stix_value
-        elif group is True:  # Mapping of multiple data fields to single STIX object field. Ex: Network Protocols
+        elif group is True: # Mapping of multiple data fields to single STIX object field. Ex: Network Protocols
             if (isinstance(child_obj[split_key[-1]], list)):
-                # append to existing list
-                child_obj[split_key[-1]].extend(stix_value)
+                child_obj[split_key[-1]].extend(stix_value)                      # append to existing list
 
     @staticmethod
     def _handle_cybox_key_def(key_to_add, observation, stix_value, obj_name_map, obj_name, group=False):
         """
         Handle the translation of the input property to its STIX CybOX property
+
         :param key_to_add: STIX property key derived from the mapping file
         :param observation: the the STIX observation currently being worked on
         :param stix_value: the STIX value translated from the input object
@@ -97,9 +98,9 @@ class DataSourceObjToStixObj:
         """
         obj_type, obj_prop = key_to_add.split('.', 1)
         objs_dir = observation['objects']
-
+        
         if obj_name in obj_name_map:
-            obj = objs_dir[obj_name_map[obj_name]]
+                obj = objs_dir[obj_name_map[obj_name]]
         else:
             obj = {'type': obj_type}
             obj_dir_key = str(len(objs_dir))
@@ -112,6 +113,7 @@ class DataSourceObjToStixObj:
     def _valid_stix_value(props_map, key, stix_value):
         """
         Checks that the given STIX value is valid for this STIX property
+
         :param props_map: the map of STIX properties which contains validation attributes
         :param key: the STIX property name
         :param stix_value: the STIX value translated from the input object
@@ -126,20 +128,19 @@ class DataSourceObjToStixObj:
                 return False
         return True
 
-    def _transform(self, object_map, observation, ds_map, ds_key, obj):
+    def _transform(self,object_map,observation,ds_map,ds_key,obj):
 
-        to_map = obj[ds_key]
+        to_map = obj[ ds_key ]
 
         if ds_key not in ds_map:
             print('{} is not found in map, skipping'.format(ds_key))
             return
 
-        if isinstance(to_map, dict):
+        if isinstance( to_map, dict ):
             print('{} is complex; descending'.format(to_map))
             # If the object is complex we must descend into the map on both sides
             for key in to_map.keys():
-                self._transform(object_map, observation,
-                                ds_map[ds_key], key, to_map)
+                self._transform(object_map,observation,ds_map[ds_key],key,to_map)
             return
 
         generic_hash_key = ''
@@ -152,8 +153,7 @@ class DataSourceObjToStixObj:
             # Use callback function to run module-specific logic to handle unknown filehash types
             if self.callback:
                 try:
-                    generic_hash_key = self.callback(
-                        obj, ds_key, ds_key_def_obj['key'], self.options)
+                    generic_hash_key = self.callback(obj, ds_key, ds_key_def_obj['key'], self.options)
                 except(Exception):
                     return
 
@@ -169,8 +169,7 @@ class DataSourceObjToStixObj:
             else:
                 key_to_add = ds_key_def['key']
 
-            transformer = self.transformers[ds_key_def['transformer']
-                                            ] if 'transformer' in ds_key_def else None
+            transformer = self.transformers[ds_key_def['transformer']] if 'transformer' in ds_key_def else None
 
             group = False
             if ds_key_def.get('cybox', self.cybox_default):
@@ -185,8 +184,7 @@ class DataSourceObjToStixObj:
                     else:
                         stix_value = object_map[references]
                 else:
-                    stix_value = DataSourceObjToStixObj._get_value(
-                        obj, ds_key, transformer)
+                    stix_value = DataSourceObjToStixObj._get_value(obj, ds_key, transformer)
                     if not DataSourceObjToStixObj._valid_stix_value(self.properties, key_to_add, stix_value):
                         continue
 
@@ -194,20 +192,18 @@ class DataSourceObjToStixObj:
                 if 'group' in ds_key_def:
                     group = True
 
-                DataSourceObjToStixObj._handle_cybox_key_def(
-                    key_to_add, observation, stix_value, object_map, object_name, group)
+                DataSourceObjToStixObj._handle_cybox_key_def(key_to_add, observation, stix_value, object_map, object_name, group)
             else:
-                stix_value = DataSourceObjToStixObj._get_value(
-                    obj, ds_key, transformer)
+                stix_value = DataSourceObjToStixObj._get_value(obj, ds_key, transformer)
                 if not DataSourceObjToStixObj._valid_stix_value(self.properties, key_to_add, stix_value):
                     continue
 
-                DataSourceObjToStixObj._add_property(
-                    observation, key_to_add, stix_value, group)
+                DataSourceObjToStixObj._add_property(observation, key_to_add, stix_value, group)
 
     def transform(self, obj):
         """
         Transforms the given object in to a STIX observation based on the mapping file and transform functions
+
         :param obj: the datasource object that is being converted to stix
         :return: the input object converted to stix valid json
         """
@@ -219,15 +215,13 @@ class DataSourceObjToStixObj:
             'id': stix_type + '--' + str(uuid.uuid4()),
             'type': stix_type,
             'created_by_ref': self.identity_id,
-            'created': "{}Z".format(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]),
-            'modified': "{}Z".format(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]),
             'objects': {}
         }
 
         # create normal type objects
-        if isinstance(obj, dict):
+        if isinstance(obj,dict):
             for ds_key in obj.keys():
-                self._transform(object_map, observation, ds_map, ds_key, obj)
+                self._transform(object_map,observation,ds_map,ds_key,obj)
         else:
             print("Not a dict: {}".format(obj))
 
