feefddaf01605d8489bc0c14da2a223d81178816
Nathan Cook <Nathan.A.Cook@ibm.com>
2019-12-27 11:21:43 -0500

    stix transmission changes

--- stix_shifter/stix_transmission/src/modules/cloudIdentity/apiclient.py
+++ stix_shifter/stix_transmission/src/modules/cloudIdentity/apiclient.py
@@ -2,19 +2,30 @@ import base64
 from ..utils.RestApiClient import RestApiClient
 from . import CloudIdentity_Request, CloudIdentity_Token
 import pprint
-import json
+import json, requests
 import re
 
 
 
 class APIClient():
-
+    PING_ENDPOINT = "v2.0/Users"
     def __init__(self, connection, configuration):
-        headers = { "Accept": "application/json, text/plain, */*","Content-Type": "application/json", "authorization": "Bearer "+CloudIdentity_Token.getToken()}
+        
+        headers = dict()
         url_modifier_function = None
         auth = configuration.get('auth')
+        headers["Accept"] = "application/json, text/plain, */*"
+        headers["Content-type"] = "application/json"
 
-        self.indices = configuration.get('elastic_ecs', {}).get('indices', None)
+        self.indices = configuration.get('cloudIdentity', {}).get('indices', None)
+
+        #Check for authentication type
+        if auth:
+           if 'tenant' in auth and 'clientId' in auth and 'clientSecret' in auth:
+               #get token from specified cloud identity tenant
+               headers["Authorization"] =  "Bearer " + self.getToken(auth['tenant'], auth['clientId'], auth['clientSecret'])
+           elif 'token' in auth: 
+               headers['Authorization'] = "Bearer " + auth['token']
 
         if isinstance(self.indices, list):  # Get list of all indices
             self.indices = ",".join(self.indices)
@@ -22,23 +33,35 @@ class APIClient():
         if self.indices:
             self.endpoint = self.indices + '/' +'_search'
         else:
-            self.endpoint = '_search'
-        
+            self.endpoint = 'v2.0/Users'
+
     def run_search(self, query_expression):
-        token = CloudIdentity_Token.getToken()
+        print("RUNNING SEARCH")
         
         ip, FROM, TO = self._parse_query(query_expression)
         try:
-            response = CloudIdentity_Request.postReports(token,"admin_activity", FROM, TO, 10, 'time', 'asc')
-            pp = pprint.PrettyPrinter(indent=1)
-            pp.pprint(response)
-            print(response)
+            #response = CloudIdentity_Request.postReports(token,"admin_activity", FROM, TO, 10, 'time', 'asc')
+            #pp = pprint.PrettyPrinter(indent=1)
+            #pp.pprint(response)
+            
 
-            return response
+            return 
         except Exception as e:
             return
 
-        
+    #Retrieve valid token from Cloud Identity
+    def getToken(self, uri, clientId, clientSecret):
+        options = {
+            'uri': uri+"/v2.0/endpoint/default/token",
+            'client_id': clientId,
+            'client_secret': clientSecret,
+            'grant_type': "client_credentials"
+        }
+        resp = requests.post(uri+"/v2.0/endpoint/default/token", data=options)
+        json_data = json.loads(resp.text)
+        token = json_data['access_token']
+        return token
+
 
     #Parse out meaningful data from input query   
     def _parse_query(self, query):
