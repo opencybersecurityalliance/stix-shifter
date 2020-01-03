9d5ba4283625b2acf463f0799c609a21ad857096
annish-py <annishprashan.stevi@hcl.com>
2020-01-03 19:35:55 +0530

    Review branch for MS Sentinel UDS connector

--- stix_shifter/stix_translation/stix_translation.py
+++ stix_shifter/stix_translation/stix_translation.py
@@ -10,7 +10,7 @@ from stix_shifter.stix_translation.src.modules.car import car_data_mapping
 from stix_shifter.stix_translation.src.utils.unmapped_attribute_stripper import strip_unmapped_attributes
 import sys
 TRANSLATION_MODULES = ['qradar', 'dummy', 'car', 'cim', 'splunk', 'elastic', 'bigfix', 'csa', 'csa:at', 'csa:nf', 'aws_security_hub', 'carbonblack',
-                       'elastic_ecs', 'proxy', 'stix_bundle', 'msatp', 'security_advisor', 'guardium']
+                       'elastic_ecs', 'proxy', 'stix_bundle', 'msatp', 'security_advisor', 'guardium', 'azure_sentinel']
 
 RESULTS = 'results'
 QUERY = 'query'
--- stix_shifter/stix_transmission/stix_transmission.py
+++ stix_shifter/stix_transmission/stix_transmission.py
@@ -2,7 +2,7 @@ import importlib
 from ..utils.error_response import ErrorResponder
 
 TRANSMISSION_MODULES = ['async_dummy', 'synchronous_dummy', 'qradar', 'splunk', 'bigfix', 'csa', 'aws_security_hub',
-                        'carbonblack', 'elastic_ecs', 'proxy', 'stix_bundle', 'msatp', 'security_advisor', 'guardium']
+                        'carbonblack', 'elastic_ecs', 'proxy', 'stix_bundle', 'msatp', 'security_advisor', 'guardium', 'azure_sentinel']
 
 
 RESULTS = 'results'
