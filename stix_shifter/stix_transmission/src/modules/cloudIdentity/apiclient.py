53ec1f0af0c53b4ce1174e54291701d2c5684d92
Nathan Cook <Nathan.A.Cook@ibm.com>
2020-01-08 12:50:36 -0500

    APIclient update: Token initialization update, now retrieves token if one doesnt exist or if current token has expired

--- stix_shifter/stix_transmission/src/modules/cloudIdentity/apiclient.py
+++ stix_shifter/stix_transmission/src/modules/cloudIdentity/apiclient.py
@@ -5,6 +5,7 @@ import urllib.parse
 import pprint
 import json, requests
 import re
+import datetime
 
 
 
@@ -17,137 +18,50 @@ class APIClient():
         self.headers = dict()
         self.search_id = None
         self.query = None 
-        self.authroization = None
-        self.credential = None
-        self.host = connection.get('host')
-        self.host_port = connection.get('port')
-        self.client = RestApiClient(self.host)
+        self.authorization = None
+        self.credentials = None
+        self.headers = dict()
+
+        self.client = RestApiClient(host=connection.get('host'), 
+                                    port=connection.get('port', None))
+        #Init connections
+        client_id = configuration.get('auth').get("clientId", None)
+        client_secret = configuration.get('auth').get("clientSecret", None)
+        tenant = configuration.get('auth').get('tenant', None)
+        self.token = configuration.get('auth').get('token', None)
+
+        #Init host/port
+        host = connection.get('host')
+        port = connection.get('port', None)
 
 
-        #Check for token 
-        if(configuration.get('auth').get('token') is not None):
-            self.token = configuration.get('auth').get('token')
-        #Check for client_id and client_secret if true initialize token
-        elif(configuration.get('auth').get("clientId") is not None 
-        and configuration.get('auth').get("clientSecret") is not None
-        and configuration.get('auth').get("tenant") is not None):
-            self.uri = configuration.get('auth').get("tenant")
-            self.token = self.getToken(self.uri, configuration.get('auth').get("clientId"),configuration.get('auth').get("clientSecret"))
-        else:
-            print("No Token initialized. Tenant client Id and client secret is required")
+        if(client_id is not None and client_secret is not None and tenant is not None):
+            self.credentials = {"tenant": tenant, "client_id": client_id, "client_secret": client_secret}
+            self.token = self.getToken()
+        else: 
+            self.credentials = None           
+            raise IOError (3001, "Cloud Identity Credentials not provided in connection/configuration")
 
+        #Init communication to Cloud Identity Tenant
         
-        
 
+        # Init base headers
+        self._add_headers('Accept', "application/json, text/plain, */*")
+        self._add_headers("Content-Type", "application/json")
 
+    # Searches both reports and events from Cloud Identity 
     def run_search(self, query_expression):
-        #Creates variables for CI
-        ip, FROM, TO = self._parse_query(query_expression)
-
-        #Different types for reports to be called 
-        report_response = self.postReports("auth_audit_trail", FROM, TO, 10, 'time', 'asc')
-        #events_response = self.getEvents(FROM, TO)
-
-
-        #Makes return json easier to read 
-        pp = pprint.PrettyPrinter(indent=1)
-
-        #refine json response to individual hits and events
-        report_hits = report_response['response']['report']['hits']
-        #event_hits = events_response['response']['events']['events']
-
-        user_response = dict()
-
-        #pp.pprint(events_response)
         
-        #Iterate over hits object
-        for index in report_hits:
-            #Compare CI report origin/ip to input IP 
-            if(str(index['_source']['data']['origin'] == ip)):
-                #REST call to CI on specified user_id
-                user_response = self.getUser(id=report_hits[0]['_source']['data']['subject'])
 
-        #pp.pprint(user_response)
+        #Run search on Cloud identity reports  
+        report_response = self.search_reports(query_expression)
+
+        #Run search on cloud identity events
+        events_response = self.search_events(query_expression)
+        
+        #TODO integrate event/report responses
         
         return report_response
-
-    #Retrieve valid token from Cloud Identity
-    def getToken(self, uri, client_id, client_secret):
-
-        options = {
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "grant_type": "client_credentials"
-        }
-
-        endpoint = "v2.0/endpoint/default/token"
-        resp = self.client.call_api(endpoint, "POST", data=options)
-
-        jresp = json.loads(str(resp.read(), 'utf-8'))
-        return jresp.get('access_token')
-        
-
-    #Returns given report from cloud identity tenant
-    def postReports(self, reportName, FROM, TO, SIZE, SORT_BY, SORT_ORDER, SEARCH_AFTER = None):
-        endpoint = "v1.0/reports/" + reportName
-
-        headers = dict()
-        headers["Accept"] = "application/json, text/plain, */*"
-        headers["Content-Type"] = "application/json"
-        headers["authorization"] = "Bearer "+ self.token
-
-        data = dict()
-        data["FROM"] = FROM
-        data["TO"] = TO
-        data["SIZE"] = SIZE
-        data["SORT_BY"] = SORT_BY
-        data["SORT_ORDER"] = SORT_ORDER
-
-        d = json.dumps(data)
-        resp = self.client.call_api(endpoint, "POST", headers = headers, data=d)
-        jresp = json.loads(str(resp.read(), 'utf-8'))
-
-        return jresp
-
-    def getEvents(self, FROM, TO):
-        
-        endpoint = f"/v1.0/events?from={FROM}&to={TO}&range_type=time"
-
-        headers = dict()
-        headers['Accept'] = "application/json, text/plain, */*"
-        headers['authorization'] = "Bearer "+self.token
-
-        response = self.client.call_api(endpoint, 'GET', headers=headers)
-        jresp = json.loads(str(response.read(), 'utf-8'))
-
-        return jresp
-       
-    def getUser(self, id):
-        endpoint = f"/v2.0/Users/{id}"
-
-        headers = dict()
-        headers['Accept'] = "application/json, text/plain, */*"
-        headers['authorization'] = "Bearer "+self.token
-
-        response = self.client.call_api(endpoint, 'GET', headers=headers)
-        jresp = json.loads(str(response.read(), 'utf-8'))
-        return jresp
-
-    #Parse out meaningful data from input query   
-    def _parse_query(self, query):
-        request = query.split(' ')
-        
-        for index in range(len(request)):
-            if(request[index] == "origin"):
-                ip = request[index+2]
-            if(request[index] == "FROM"):
-                FROM = request[index+1]
-            if(request[index] ==  "TO"):
-                TO = request[index+1]
-        
-        FROM = FROM.strip("t''")
-        TO = TO.strip("t''")
-        return ip, FROM, TO
 
     def ping_data_source(self):
         # Pings the data source
@@ -173,3 +87,166 @@ class APIClient():
         # Optional since this may not be supported by the data source API
         # Delete the search
         return "Deleted query: {}".format(search_id)
+
+
+    #NOTE All functions below are either Cloud Identity REST calls or modifier functions 
+
+
+    def search_reports(self, query_expression):
+        #Creates variables for CI
+        ip, FROM, TO = self._parse_query(query_expression)
+
+        resp = self.postReports("auth_audit_trail", FROM, TO, 10, 'time', 'asc')
+        #refine json response to individual hits and events
+        report_hits = resp['response']['report']['hits']
+
+        #Iterate over hits object
+        for index in report_hits:
+            #Compare CI report origin/ip to input IP 
+            if(str(index['_source']['data']['origin'] == ip)):
+                #REST call to CI on specified user_id
+                user_response = self.getUser(id=report_hits[0]['_source']['data']['subject'])
+
+        #Makes return json easier to read 
+        #pp = pprint.PrettyPrinter(indent=1)
+        #pp.pprint(user_response)
+
+    def search_events(self, query_expression):
+        #Creates variables for CI
+        ip, FROM, TO = self._parse_query(query_expression)
+
+        resp = self.getEvents(FROM, TO)
+        #refine json response to individual hits and events
+        event_hits = resp['response']['events']['events']
+
+        #Iterate over hits object
+        #for index in event_hits:
+            #Compare CI report origin/ip to input IP 
+            #if(str(index['_source']['data']['origin'] == ip)):
+                #REST call to CI on specified user_id
+                #user_response = self.getUser(id=event_hits[0]['_source']['data']['subject'])
+
+        #Makes return json easier to read 
+        #pp = pprint.PrettyPrinter(indent=1)
+        #pp.pprint(user_response)
+        return event_hits
+
+    def get_credentials(self):
+        if self.credentials is None:
+            raise IOError(3001, "Cloud Identity Credential object is None")
+        else: 
+            data = self.credentials
+
+        return data
+
+    #Retrieve valid token from Cloud Identity
+    def getToken(self):
+
+        success = False
+        if(self.authorization is not None):
+            if (self.isTokenExpired((self.authorization).get("expiresTimestamp"))):
+                success = True 
+                self._addAuthHeader()
+                return success
+
+        
+        if(self._getToken()):
+            success = True
+            self._addAuthHeader()
+
+        return success
+
+    def _getToken(self):
+        success = False
+
+        auth = self.get_credentials()
+        #print(auth)
+
+        options = {
+            "client_id": auth.get("client_id"),
+            "client_secret": auth.get("client_secret"),
+            "grant_type": "client_credentials"
+        }
+
+        endpoint = "v2.0/endpoint/default/token"
+
+        time = datetime.datetime.now()
+        resp = self.client.call_api(endpoint, "POST", data=options)
+        jresp = json.loads(str(resp.read(), 'utf-8')) 
+
+        print(jresp)
+
+        if(resp.code != 200):
+            raise ValueError(3002, str(jresp) + " -- Access Token not received")
+        else:
+            success = True
+            exTime = (time + datetime.timedelta(seconds=jresp.get("expires_in"))).timestamp()
+            self.authorization = json.loads('{"access_token":"' + jresp.get("access_token") + '", "expiresTimestamp":' + str(exTime) + '}')
+
+        print(self.authorization)
+        return success
+        
+    def isTokenExpired(self, exTime):
+        if exTime is not None:
+            if(exTime > (datetime.datetime.now()).timestamp()):
+                return True
+        return False
+
+    #Returns given report from cloud identity tenant
+    def postReports(self, reportName, FROM, TO, SIZE, SORT_BY, SORT_ORDER, SEARCH_AFTER = None):
+        endpoint = "v1.0/reports/" + reportName
+
+        data = dict()
+        data["FROM"] = FROM
+        data["TO"] = TO
+        data["SIZE"] = SIZE
+        data["SORT_BY"] = SORT_BY
+        data["SORT_ORDER"] = SORT_ORDER
+
+        data = json.dumps(data)
+        resp = self.client.call_api(endpoint, "POST", headers = self.headers, data=data)
+        jresp = json.loads(str(resp.read(), 'utf-8'))
+
+        return jresp
+
+    def getEvents(self, FROM, TO):
+        
+        endpoint = f"/v1.0/events?&from={FROM}&to={TO}&range_type=time"
+
+        response = self.client.call_api(endpoint, 'GET', headers=self.headers)
+        jresp = json.loads(str(response.read(), 'utf-8'))
+
+        return jresp
+       
+    def getUser(self, id):
+        endpoint = f"/v2.0/Users/{id}"
+
+        response = self.client.call_api(endpoint, 'GET', headers=self.headers)
+        jresp = json.loads(str(response.read(), 'utf-8'))
+
+        return jresp
+
+    #Parse out meaningful data from input query   
+    def _parse_query(self, query):
+        request = query.split(' ')
+        
+        for index in range(len(request)):
+            if(request[index] == "origin"):
+                ip = request[index+2]
+            if(request[index] == "FROM"):
+                FROM = request[index+1]
+            if(request[index] ==  "TO"):
+                TO = request[index+1]
+        
+        FROM = FROM.strip("t''")
+        TO = TO.strip("t''")
+        return ip, FROM, TO
+
+    def _add_headers(self, key, value):
+        self.headers[key] = value
+        return
+
+    def _addAuthHeader(self):
+        auth = "Bearer " + str(self.authorization.get("access_token"))
+        self._add_headers("authorization", auth)
+        return
