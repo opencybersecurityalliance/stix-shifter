d59ebb5971497d65128a535ace82e13b0e8da9b1
Danny Elliott <danny.elliott@ibm.com>
2020-02-06 17:30:37 -0400

    payload mapping changes

--- tests/stix_translation/test_qradar_json_to_stix.py
+++ tests/stix_translation/test_qradar_json_to_stix.py
@@ -3,6 +3,7 @@ from stix_shifter.stix_translation.src.utils import transformers
 from stix_shifter.stix_translation.src.modules.qradar import qradar_translator
 import json
 from stix_shifter.stix_translation import stix_translation
+import base64
 
 interface = qradar_translator.Translator()
 map_file = open(interface.mapping_filepath).read()
@@ -58,7 +59,8 @@ class TestTransform(object):
         assert(observed_data['last_observed'] is not None)
 
     def test_cybox_observables(self):
-        payload = "SomeBase64Payload"
+        payload = "Payload"
+        encoded_string = base64.b64encode(payload.encode('ascii')).decode('ascii')
         user_id = "someuserid2018"
         url = "https://example.com"
         source_ip = "fd80:655e:171d:30d4:fd80:655e:171d:30d4"
@@ -66,7 +68,7 @@ class TestTransform(object):
         file_name = "somefile.exe"
         source_mac = "00-00-5E-00-53-00"
         destination_mac = "00-00-5A-00-55-01"
-        data = {"sourceip": source_ip, "destinationip": destination_ip, "url": url, "base64_payload": payload, "username": user_id, "protocol": 'TCP',
+        data = {"sourceip": source_ip, "destinationip": destination_ip, "url": url, "payload": payload, "username": user_id, "protocol": 'TCP',
                 "sourceport": "3000", "destinationport": 2000, "filename": file_name, "domainname": url, "sourcemac": source_mac, "destinationmac": destination_mac}
 
         result_bundle = json_to_stix_translator.convert_to_stix(
@@ -110,7 +112,7 @@ class TestTransform(object):
         curr_obj = TestTransform.get_first_of_type(objects.values(), 'artifact')
         assert(curr_obj is not None), 'artifact object type not found'
         assert(curr_obj.keys() == {'type', 'payload_bin'})
-        assert(curr_obj['payload_bin'] == payload)
+        assert(curr_obj['payload_bin'] == encoded_string)
 
         curr_obj = TestTransform.get_first_of_type(objects.values(), 'user-account')
         assert(curr_obj is not None), 'user-account object type not found'
