60e1a343e3f95e101d8af52b4fab23aa59d0d35b
subash0993 <subash081993@gmail.com>
2020-02-13 17:11:12 +0530

    aws_cwl query connector loggroups limit

--- stix_shifter/stix_transmission/src/modules/aws_cloud_watch_logs/aws_cloud_watch_logs_query_connector.py
+++ stix_shifter/stix_transmission/src/modules/aws_cloud_watch_logs/aws_cloud_watch_logs_query_connector.py
@@ -2,6 +2,13 @@ from ..base.base_query_connector import BaseQueryConnector
 from .....utils.error_response import ErrorResponder
 import json
 
+# aws maximum limit for start query loggroups
+LOG_GROUP_NAMES_LIMIT = 20
+
+
+class InvalidParameterException(Exception):
+    pass
+
 
 class AWSCloudWatchLogsQueryConnector(BaseQueryConnector):
 
@@ -30,13 +37,16 @@ class AWSCloudWatchLogsQueryConnector(BaseQueryConnector):
                     query['logGroupNames'] = log_groups
                 else:
                     query['logGroupNames'] = [log_groups]
+                # checking if loggroups is greater than maximum limit 20
+                if len(query['logGroupNames']) > LOG_GROUP_NAMES_LIMIT:
+                    raise InvalidParameterException("Too many log groups specified, log groups maximum limit is 20")
             # if loggroupnames is none, describe_log_groups api will be called
             else:
                 log_group_response_dict = self.client.describe_log_groups(**{})
                 log_group_names = []
                 for log_group in log_group_response_dict['logGroups']:
                     log_group_names.append(log_group['logGroupName'])
-                query['logGroupNames'] = log_group_names
+                query['logGroupNames'] = log_group_names[:LOG_GROUP_NAMES_LIMIT]
             query.pop('logType')
             response_dict = self.client.start_query(**query)
             return_obj['success'] = True
