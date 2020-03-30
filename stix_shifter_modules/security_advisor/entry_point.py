aef04870d2d7ea74afa3b87cb3f910d172623bd3
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2020-03-30 13:31:29 -0300

    add JSONToStixObservablesDecorator

--- stix_shifter_modules/security_advisor/entry_point.py
+++ stix_shifter_modules/security_advisor/entry_point.py
@@ -1,10 +1,12 @@
 from stix_shifter_utils.utils.entry_point_base import EntryPointBase
 from .stix_translation.security_advisor_query_translator import SecurityAdvisorQueryTranslator
+from .stix_translation.security_advisor_results_translator import JSONToStixObservablesDecorator
 from .stix_transmission.security_advisor_ping_connector import SecurityAdvisorPingConnector
 from .stix_transmission.security_advisor_query_connector import SecurityAdvisorQueryConnector
 from .stix_transmission.security_advisor_status_connector import SecurityAdvisorStatusConnector
 from .stix_transmission.security_advisor_results_connector import SecurityAdvisorResultsConnector
 from .stix_transmission.security_advisor_delete_connector import SecurityAdvisorDeleteConnector
+from os import path
 
 class EntryPoint(EntryPointBase):
 
@@ -27,4 +29,7 @@ class EntryPoint(EntryPointBase):
             self.set_query_connector(query_connector)
             self.set_ping_connector(ping_connector)
         else:
-            self.add_dialect('default', query_translator=SecurityAdvisorQueryTranslator(), data_mapper=None, default=True)
+            basepath = path.dirname(__file__)
+            filepath = path.abspath(path.join(basepath, "stix_translation", "json", "to_stix_map.json"))
+            results_translator = JSONToStixObservablesDecorator(filepath)
+            self.add_dialect('default', query_translator=SecurityAdvisorQueryTranslator(), results_translator=results_translator, data_mapper=None, default=True)
