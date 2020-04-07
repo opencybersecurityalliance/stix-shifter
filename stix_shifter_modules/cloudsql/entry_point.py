4c399b637aef54c4047e086924f14981601feb87
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2020-04-07 17:23:48 -0300

    shuffle lines to stay in one order

--- stix_shifter_modules/cloudsql/entry_point.py
+++ stix_shifter_modules/cloudsql/entry_point.py
@@ -1,10 +1,10 @@
 from stix_shifter_utils.utils.entry_point_base import EntryPointBase
+from ibmcloudsql import SQLQuery
 from .stix_transmission.cloudsql_ping_connector import CloudSQLPingConnector
 from .stix_transmission.cloudsql_query_connector import CloudSQLQueryConnector
 from .stix_transmission.cloudsql_status_connector import CloudSQLStatusConnector
 from .stix_transmission.cloudsql_results_connector import CloudSQLResultsConnector
 from .stix_transmission.cloudsql_delete_connector import CloudSQLDeleteConnector
-from ibmcloudsql import SQLQuery
 
 import json
 
@@ -21,15 +21,14 @@ class EntryPoint(EntryPointBase):
             api_client = SQLQuery(auth["bxapikey"], instance_crn, target_cos,
                                     client_info=client_info)
             api_client.logon()
-            results_connector = CloudSQLResultsConnector(api_client)
-            status_connector = CloudSQLStatusConnector(api_client)
-            query_connector = CloudSQLQueryConnector(api_client)
             ping_connector = CloudSQLPingConnector(api_client)
+            query_connector = CloudSQLQueryConnector(api_client)
+            status_connector = CloudSQLStatusConnector(api_client)
+            results_connector = CloudSQLResultsConnector(api_client)
             delete_connector = CloudSQLDeleteConnector(api_client)
 
-            self.set_results_connector(results_connector)
-            self.set_status_connector(status_connector)
-            self.set_delete_connector(delete_connector)
-            self.set_query_connector(query_connector)
             self.set_ping_connector(ping_connector)
-        
+            self.set_query_connector(query_connector)
+            self.set_status_connector(status_connector)
+            self.set_results_connector(results_connector)
+            self.set_delete_connector(delete_connector)
