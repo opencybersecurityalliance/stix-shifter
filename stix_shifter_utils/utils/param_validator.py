70d70a4d32b5e9e6d9c134579c2db04de1a057c3
Md Azam <mdazam@ca.ibm.com>
2020-04-16 18:41:13 -0300

    update3

--- stix_shifter_utils/utils/param_validator.py
+++ stix_shifter_utils/utils/param_validator.py
@@ -9,21 +9,10 @@ def modernize_objects(module, params):
             configs = json.load(mapping_file)
     except Exception as ex:
         raise(ex)
-    expected_connection = configs.get("connection")
-    # for key, value in expected_connection.items():
-    #     if key 
 
     modernize_step(configs, params, params)
 
-    # if isinstance(params, dict):
-    #     if "options" in params.keys():
-    #         options = params.get('options')
-    #         if isinstance(options, dict):
-    #             for options_key, options_value in options.items():
-    #                 params[options_key] = options_value
-    #             params.pop("options")
-
-    # return params
+    return params
 
 def modernize_step(configs, params, params_root):
     if isinstance(configs, dict):
@@ -62,83 +51,77 @@ def del_dot_path(params, path):
     else:
         del params[path]
 
-def param_validator(module, connection, configuration):
-    valid = False
+def param_validator(module, input_configs):
 
     config_json_path = f'stix_shifter_modules/{module}/configuration/{module}_config.json'
     
     try:
         with open(config_json_path) as mapping_file:
-            configs = json.load(mapping_file)
+            expected_configs = json.load(mapping_file)
     except Exception as ex:
         raise(ex)
     
-    # if isinstance(configs, dict):
-    for key, value in configs.items():
-        if key == 'connection':
-            expected_connection = configs.get("connection")
-            valid = validate(connection, expected_connection)
-        elif key == 'configuration':
-            expected_configuration = configs.get("configuration")["auth"]
-            valid = validate(configuration["auth"], expected_configuration)
+    # if isinstance(expected_configs, dict):
+    #     if expected_configs['connection']:
+    #         return_obj = {}
+    #         validate(connection, expected_configs['connection'], return_obj)
+    #         print(str(return_obj))
+    #         if connection:
+    #             for k in connection.keys():
+    #                 print('Invalid param: ' + str(k))
+    #     if expected_configs['configuration']:
+    #          validate(configuration, expected_configs['configuration'], return_obj)
+    #          if configuration:
+    #             for k in configuration.keys():
+    #                 print('Invalid param: ' + str(k))
+    #          print(str(return_obj))
+    validated_params = {}
+    validate(input_configs, expected_configs, validated_params)
 
-    return valid
 
-def validate(params, expected_params):
-    valid = False
-    0. rework param_validator to a 1 call validation for configuration and connection (recurtion, options may contain only objects from the root level but everything is optional)
-    1. all the keys(required) from json are present in udi config_json_path (implemented)
-    2. no extra params are provided, except options
-        |
-         --->   on step 1
-                during validation MOVE from udi_conf_dict to udi_conf_validated_dict
-                make sure udi_conf_dict has no extra params 
+def validate(input_configs, expected_configs, validated_params):
 
-    if isinstance(params, dict):
-        for key, value in expected_params.items():
-            if key in params.keys():
-                valid = True
-            elif value.get('optional'):
-                continue
-            else:
-                raise Exception("Parameter validation failed: parameter \"{}\" not found".format(key))
-    else:
-        raise Exception("Parameter validation failed: Invalid parameter object \"{}\" passed in the query".format(params))
-    
-    return valid
+    # 0. rework param_validator to a 1 call validation for configuration and connection (recurtion, options may contain only objects from the root level but everything is optional)
+    # 1. all the keys(required) from json are present in udi config_json_path (implemented)
+    # 2. no extra params are provided, except options
+    #     |
+    #      --->   on step 1
+    #             during validation MOVE from udi_conf_dict to udi_conf_validated_dict
+    #             make sure udi_conf_dict has no extra params 
 
-# def validate(params, expected_params):
-#     valid = False
+    # udi_conf_dict = {} 
+    # udi_conf_validated_dict = {}
 
-#     if isinstance(params, dict):
-#         for key, value in params.items():
-#             print('KEY' + str(key))
-#             print('VALUE' + str(value))
-#             # TODO: data type match
-#             if isinstance(value, dict):
-#                 print('VALUE2' + str(value))
-#                 valid = validate(value, expected_params)
-#             elif key in expected_params.keys():
-#                 valid = True
-#             elif expected_params.get(key)['optional']:
-#                 continue
-#             else:
-#                 raise Exception("Parameter validation failed: Invalid parameter \"{}\" passed in the query".format(key))
-#     else:
-#         raise Exception("Parameter validation failed: Invalid parameter object \"{}\" passed in the query".format(params))
-    
-#     return valid
-
+    if isinstance(expected_configs, dict):
+        for key, value in expected_configs.items():
+            if key in input_configs:
+                if isinstance(value, dict):
+                    validate(input_configs[key], value, validated_params)
+                
+                if key in input_configs:
+                    validated_params[key] = input_configs[key]
+                    del input_configs[key]
+                
 
 if __name__ == "__main__":
-    print('hello')
 
     udi_conf = {'connection': {'options': {'region': 'Labrador', 'region2': 'Nova Scotia'}}}
     rules = {'connection': {'region': {'type': 'text', 'previous': 'connection.options.region'}, 'region2': {'type': 'text', 'previous': 'connection.options.region2'}}}
     # value = get_dot_path(udi_conf, 'connection.options.region')
     # print(value)
-    modernize_step(rules, udi_conf, udi_conf)
-    print('=================')
-    print(str(udi_conf))
+    # modernize_step(rules, udi_conf, udi_conf)
+    # print('=================')
+    # print(str(udi_conf))
+
+    input_params = {'connection': {'region': 'us-east'} }
+    module = 'aws_cloud_watch_logs'
+    config_json_path = f'stix_shifter_modules/{module}/configuration/{module}_config.json'
     
-     
+    try:
+        with open(config_json_path) as mapping_file:
+            expected_configs = json.load(mapping_file)
+    except Exception as ex:
+        raise(ex)
+    input_configs = {'configuration': {'aws_access_key_id': 'AKIA6IBDIZS3EIAR5RM5', 'aws_iam_role': 'arn:aws:iam::979326..._api_role', 'aws_secret_access_key': 'p0qAfOlxJ/zd0yqhj/D...ssoF+A0Cd'}, 'connection': {'k1': 'v1', 'region': 'us-east-1'}}
+    validated_params = {}
+    validate(input_configs, expected_configs, validated_params)
