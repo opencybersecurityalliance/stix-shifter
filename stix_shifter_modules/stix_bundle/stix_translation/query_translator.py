567dab4003bc58c96f3d3fec36d47662ae7733e0
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2020-07-09 14:06:52 -0300

    fix stix_bundle query_translation

--- stix_shifter_modules/stix_bundle/stix_translation/query_translator.py
+++ stix_shifter_modules/stix_bundle/stix_translation/query_translator.py
@@ -1,9 +1,10 @@
-from stix_shifter_utils.modules.base.stix_translation.base_query_translator import BaseQueryTranslator
+from stix_shifter_utils.modules.base.stix_translation.empty_query_translator import EmptyQueryTranslator
 import re
 
 START_STOP_PATTERN = "\s?START\s?t'\d{4}(-\d{2}){2}T\d{2}(:\d{2}){2}(\.\d+)?Z'\sSTOP\s?t'\d{4}(-\d{2}){2}T(\d{2}:){2}\d{2}.\d{1,3}Z'\s?"
 
-class QueryTranslator(BaseQueryTranslator):
+
+class QueryTranslator(EmptyQueryTranslator):
 
     def transform_query(self, data, antlr_parsing_object={}):
         # Data is a STIX pattern.
