d167b3a5f89db6a5c947848b1fa4999e8747fcd9
Guy  Galil <guyga@il.ibm.com>
2020-08-07 11:38:11 +0300

    guardium-dev-2

--- stix_shifter_modules/guardium/stix_transmission/error_mapper.py
+++ stix_shifter_modules/guardium/stix_transmission/error_mapper.py
@@ -3,28 +3,27 @@ from stix_shifter_utils.utils.error_response import ErrorCode
 from stix_shifter_utils.utils import logger
 
 error_mapping = {
-        #search does not exist
-        1002: ErrorCode.TRANSMISSION_SEARCH_DOES_NOT_EXISTS,
-        # The search cannot be created. The requested search ID that was provided in the query expression is already in use.
-        # Please use a unique search ID (or allow one to be generated).
-        1004: ErrorCode.TRANSMISSION_MODULE_DEFAULT_ERROR.value,
-        # A request parameter is not valid
-        1005: ErrorCode.TRANSMISSION_INVALID_PARAMETER,
-        #No Guardium records returned 
-        1010: ErrorCode.TRANSMISSION_RESPONSE_EMPTY_RESULT,
-        #Not Used
-        1020: ErrorCode.TRANSMISSION_MODULE_DEFAULT_ERROR.value,
-        #Used 
-        2000: ErrorCode.TRANSMISSION_QUERY_PARSING_ERROR,
-        #error in AUTHORIZATION CREDENTIAL
-        3001: ErrorCode.TRANSMISSION_AUTH_CREDENTIALS,
-        #error in ACCESS TOKEN MISSING
-        3002: ErrorCode.TRANSMISSION_AUTH_CREDENTIALS
-    }
+    # These are only examples. Change the keys to reflect the error codes that come back from the data source API.
+    # search does not exist
+    1002: ErrorCode.TRANSMISSION_SEARCH_DOES_NOT_EXISTS,
+    # The search cannot be created. The requested search ID that was provided in the query expression is already in use.
+    # Please use a unique search ID (or allow one to be generated).
+    1004: ErrorCode.TRANSMISSION_MODULE_DEFAULT_ERROR.value,
+    # A request parameter is not valid
+    1005: ErrorCode.TRANSMISSION_INVALID_PARAMETER,
+    # The server might be temporarily unavailable or offline. Please try again later.
+    1010: ErrorCode.TRANSMISSION_REMOTE_SYSTEM_IS_UNAVAILABLE,
+    # An error occurred during the attempt
+    1020: ErrorCode.TRANSMISSION_MODULE_DEFAULT_ERROR.value,
+    #error in query
+    2000: ErrorCode.TRANSMISSION_QUERY_PARSING_ERROR
+}
+
 
 class ErrorMapper():
-    logger = logger.set_logger(__name__)
+
     DEFAULT_ERROR = ErrorCode.TRANSMISSION_MODULE_DEFAULT_ERROR
+    logger = logger.set_logger(__name__)
 
     @staticmethod
     def set_error_code(json_data, return_obj):
@@ -40,6 +39,6 @@ class ErrorMapper():
             error_code = error_mapping[code]
 
         if error_code == ErrorMapper.DEFAULT_ERROR:
-            ErrorMapper.logger.error("failed to map: "+ str(json_data))
+            ErrorMapper.logger.error("failed to map: " + str(json_data))
 
         ErrorMapperBase.set_error_code(return_obj, error_code)
