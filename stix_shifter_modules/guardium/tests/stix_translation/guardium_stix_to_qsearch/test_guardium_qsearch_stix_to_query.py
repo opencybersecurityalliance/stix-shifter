407280fc11e871dbe4c1df14363d137bff81844f
adirb <adirb@il.ibm.com>
2020-08-16 21:52:51 +0300

    Set severity as x-guardium

--- stix_shifter_modules/guardium/tests/stix_translation/guardium_stix_to_qsearch/test_guardium_qsearch_stix_to_query.py
+++ stix_shifter_modules/guardium/tests/stix_translation/guardium_stix_to_qsearch/test_guardium_qsearch_stix_to_query.py
@@ -1,4 +1,7 @@
+
 from stix_shifter.stix_translation import stix_translation
+from stix_shifter_utils.utils.error_response import ErrorCode
+from stix_shifter.stix_translation.stix_translation import MAPPING_ERROR
 import unittest
 
 
@@ -53,4 +56,21 @@ class TestQueryTranslator(unittest.TestCase, object):
         _test_query_assertions(query['queries'], 0, filters)
         filters = "name=DBUser&value=MARCI&isGroup=false"
         _test_query_assertions(query['queries'], 0, filters)
+
+    def test_query_or_with_non_qs_field(self):
+        stix_pattern = "[ unmapped-object:some_invalid_attribute = 'whatever' OR ipv4-addr:value = '1.2.3.4']"
+        query = _translate_query(stix_pattern)
+        filters = "\"filters\":\"name=Server&value=1.2.3.4&isGroup=false\""
+        _test_query_assertions(query['queries'], 0, filters)
+        assert 1 == len(query['queries'])
+
+    def test_query_and_with_non_qs_field(self):
+        stix_pattern = "[ unmapped-object:some_invalid_attribute = 'whatever' AND ipv4-addr:value = '1.2.3.4']"
+        query = _translate_query(stix_pattern)
+        assert not query['success']
+        assert ErrorCode.TRANSLATION_MAPPING_ERROR.value == query['code']
+        assert MAPPING_ERROR in query['error']
+
+
+
 
