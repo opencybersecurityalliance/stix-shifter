53b3b6fcabef3280837d66f0fca07ccc6d42d408
Yurii Klymenko <11822597+yurii-klymenko@users.noreply.github.com>
2020-10-05 15:36:42 -0400

    Retry limit 3 (#457)
    remove ping_timout, retry=3 used instead.

--- stix_shifter_modules/proxy/stix_transmission/connector.py
+++ stix_shifter_modules/proxy/stix_transmission/connector.py
@@ -1,44 +1,45 @@
-from stix_shifter_utils.modules.base.stix_transmission.base_connector import BaseConnector
-import json
 import requests
 import copy
-
+import json
+from stix_shifter_utils.modules.base.stix_transmission.base_connector import BaseConnector
+from stix_shifter_utils.stix_transmission.utils.RestApiClient import RestApiClient
 
 class Connector(BaseConnector):
     def __init__(self, connection, configuration):
         self.request_http_path = "http://{}:{}".format(connection['options']['proxy_host'], connection['options']['proxy_port'])
         self.timeout = connection['options']['timeout']
         self.connection, self.configuration = self._unwrap_connection_options(copy.deepcopy(connection), copy.deepcopy(configuration))
-
+        self.client = RestApiClient(connection['options']['proxy_host'], connection['options']['proxy_port'], url_modifier_function=lambda host_port,endpoint,headers: f'http://{host_port}{endpoint}')
+        
     def ping_connection(self):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration})
-        response = requests.post(self.request_http_path + "/ping", data, timeout=self.timeout)
-        return response.json()
+        response = self.client.call_api('/ping', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def create_query_connection(self, query):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration, "query": query})
-        response = requests.post(self.request_http_path + "/create_query_connection", data, timeout=self.timeout)
-        return response.json()
+        response = self.client.call_api('/create_query_connection', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def create_results_connection(self, search_id, offset, length):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration, "search_id": search_id, "offset": offset, "length": length})
-        response = requests.post(self.request_http_path + "/create_results_connection", data, timeout=self.timeout)
-        return response.json()
+        response = self.client.call_api('/create_results_connection', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def create_status_connection(self, search_id):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration, "search_id": search_id})
-        response = requests.post(self.request_http_path + "/create_status_connection", data, timeout=self.timeout)
-        return response.json()
+        response = self.client.call_api('/create_status_connection', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def delete_query_connection(self, search_id):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration, "search_id": search_id})
-        response = requests.post(self.request_http_path + "/delete_query_connection", data, timeout=self.timeout)
-        return response.json()
+        response = self.client.call_api('/delete_query_connection', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def is_async(self):
         data = json.dumps({"connection": self.connection, "configuration": self.configuration})
-        response = requests.post(self.request_http_path + "/is_async", data, timeout=self.timeout)
-        return response.text
+        response = self.client.call_api('/is_async', 'POST', data=data, timeout=self.timeout)
+        return json.loads(response.bytes)
 
     def _unwrap_connection_options(self, connection, configuration):
         if 'options' in connection and 'destination' in connection['options']:
