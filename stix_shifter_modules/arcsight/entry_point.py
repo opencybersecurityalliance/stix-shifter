cfb737ed77cc85addd808f508796f501c8610f52
annish-py <annishprashan.stevi@hcl.com>
2020-11-03 12:17:09 +0530

    Initial PR Commit for ISC UDS Microfocus ArcSight

---
+++ stix_shifter_modules/arcsight/entry_point.py
@@ -0,0 +1,12 @@
+from stix_shifter_utils.utils.base_entry_point import BaseEntryPoint
+
+
+class EntryPoint(BaseEntryPoint):
+
+    def __init__(self, connection={}, configuration={}, options={}):
+        super().__init__(connection, configuration, options)
+
+        if connection and configuration:
+            self.setup_transmission_simple(connection, configuration)
+        else:
+            self.setup_translation_simple(dialect_default='default')
