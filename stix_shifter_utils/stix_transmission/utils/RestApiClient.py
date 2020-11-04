b1b76e41e1a924174cfc32dd5b3014d41b25491a
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2020-11-04 11:26:43 -0500

    debug requests

--- stix_shifter_utils/stix_transmission/utils/RestApiClient.py
+++ stix_shifter_utils/stix_transmission/utils/RestApiClient.py
@@ -9,12 +9,26 @@ import os
 import errno
 import uuid
 import datetime
-
+import logging
 from stix_shifter_utils.utils import logger
 
 # This is a simple HTTP client that can be used to access the REST API
 
 RETRY_MAX = 3
+
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 
 class RestApiClient:
