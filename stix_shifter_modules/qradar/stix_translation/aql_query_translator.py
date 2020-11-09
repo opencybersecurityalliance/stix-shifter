d07e719f5465f831e830298adf513420e01cd8c6
Md Azam <mdazam@ca.ibm.com>
2020-11-09 16:27:11 -0400

    User time_range from options when no time specified in the query

--- stix_shifter_modules/qradar/stix_translation/aql_query_translator.py
+++ stix_shifter_modules/qradar/stix_translation/aql_query_translator.py
@@ -44,6 +44,17 @@ class AqlQueryTranslator(BaseQueryTranslator):
                 if result.get(START_TIME) and not result.get(END_TIME):
                     current_time = datetime.now()
                     result[END_TIME] = int(float(datetime.strptime(str(current_time), '%Y-%m-%d %H:%M:%S.%f').strftime('%s.%f'))*1000)
+        
+        # User time_range from options when no time specified in the query 
+        if not result.get(START_TIME) and not result.get(END_TIME):
+            time_range = self.options.get('time_range')
+            current_time = datetime.now()
+            before_time = current_time - timedelta(minutes=int(time_range))
+            start_dt_obj = datetime.strptime(str(before_time), '%Y-%m-%d %H:%M:%S.%f').strftime('%s.%f')
+            result[START_TIME] = int(float(start_dt_obj)*1000)
+            stop_dt_obj = datetime.strptime(str(current_time), '%Y-%m-%d %H:%M:%S.%f').strftime('%s.%f')
+            result[END_TIME] = int(float(stop_dt_obj)*1000)
+
         return result
 
     def get_language(self):
