e18f58ba6d808c08cd594047e29f02c777cc23fd
Md Azam <mdazam@ca.ibm.com>
2021-01-07 09:44:51 -0400

    Remove codes for binary searches

--- stix_shifter_modules/carbonblack/stix_translation/query_translator.py
+++ stix_shifter_modules/carbonblack/stix_translation/query_translator.py
@@ -9,27 +9,6 @@ logger = logging.getLogger(__name__)
 
 
 class QueryTranslator(BaseQueryTranslator):
-    def __init__(self, options, dialect, basepath):
-        super().__init__(options, dialect, basepath)
-        assert len(self.map_data) == 2
-        assert "process" in self.map_data and "binary" in self.map_data
-
-    def fetch_mapping(self, not_used_path, not_used_dialect, not_used_options):
-        process_mapping = read_json("process_from_stix_map.json", self.options)
-        binary_mapping = read_json("binary_from_stix_map.json", self.options)
-        return {"binary": binary_mapping, "process": process_mapping}
-
-    def map_field(self, stix_object_name, stix_property_name):
-        results = []
-        for dialect in ["binary", "process"]:  # binary api fields mappings are preferred
-            mapping = self.map_data[dialect]
-            if stix_object_name in mapping and stix_property_name in mapping[stix_object_name]["fields"]:
-                results.append((mapping[stix_object_name]["fields"][stix_property_name], dialect))
-
-        if len(results) != 0:
-            return results
-        else:
-            return []
 
     def transform_antlr(self, data, antlr_parsing_object):
         """
