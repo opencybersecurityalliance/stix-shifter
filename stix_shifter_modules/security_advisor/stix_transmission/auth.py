7b0b528a4309d605d0216099975195130c06e68a
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-03-15 18:21:45 +0530

    admin api url and iam url configurable

--- stix_shifter_modules/security_advisor/stix_transmission/auth.py
+++ stix_shifter_modules/security_advisor/stix_transmission/auth.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 class Auth():
     def __init__(self, apiKey):
@@ -9,7 +10,7 @@ class Auth():
         if(not self.apiKey):
             raise Exception("Authorizaion Failed")
 
-        iamTokenURL = 'https://iam.cloud.ibm.com/identity/token'
+        iamTokenURL = os.getenv('IAM_URL', 'https://iam.cloud.ibm.com/identity/token')
         requestBody = "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={}&response_type=cloud_iam".format(self.apiKey)
 
         header = {
@@ -28,10 +29,11 @@ class Auth():
             raise Exception("Authorizaion Failed")
 
     def validate_location(self, accountID, host):
+        adminApiUrl = os.getenv('ADMIN_API_URL', 'https://compliance.cloud.ibm.com/admin/v1')
         headers = {"Authorization": "Bearer {0}".format(self.obtainAccessToken())}
-        resp = requests.get("https://dev.compliance.test.cloud.ibm.com/admin/v1/accounts/{0}/settings".format(accountID), headers=headers)
+        resp = requests.get("{0}/accounts/{1}/settings".format(adminApiUrl, accountID), headers=headers)
         current_location_id = resp.json()["location"]["id"]
-        resp = requests.get("https://dev.compliance.test.cloud.ibm.com/admin/v1/locations/{0}".format(current_location_id), headers=headers)
+        resp = requests.get("{0}/locations/{1}".format(adminApiUrl, current_location_id), headers=headers)
         actual_service_url = resp.json()["si_findings_endpoint_url"]
         if "{0}/v1".format(actual_service_url) != host:
             raise ValueError("The service URL you specified is incorrect for the location selected for the account. The correct URL is: {0}/v1".format(actual_service_url))
