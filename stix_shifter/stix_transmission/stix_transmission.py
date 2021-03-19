48e34545c72f19725ec7140fe0ef3f9b4233109a
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2021-03-19 08:37:19 -0400

    cleanup

--- stix_shifter/stix_transmission/stix_transmission.py
+++ stix_shifter/stix_transmission/stix_transmission.py
@@ -29,7 +29,7 @@ class StixTransmission:
     def query(self, query):
         # Creates and sends a query to the correct datasource
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.create_query_connection(query)
         except Exception as ex:
@@ -40,7 +40,7 @@ class StixTransmission:
     def status(self, search_id):
         # Creates and sends a status query to the correct datasource asking for the status of the specific query
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.create_status_connection(search_id)
         except Exception as ex:
@@ -51,7 +51,7 @@ class StixTransmission:
     def results(self, search_id, offset, length):
         # Creates and sends a query to the correct datasource asking for results of the specific query
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.create_results_connection(search_id, offset, length)
         except Exception as ex:
@@ -61,7 +61,7 @@ class StixTransmission:
 
     def results_stix(self, search_id, offset, length, data_source):
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.create_results_stix_connection(search_id, offset, length, data_source)
         except Exception as ex:
@@ -72,7 +72,7 @@ class StixTransmission:
     def delete(self, search_id):
         # Sends a request to the correct datasource, asking to terminate a specific query
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.delete_query_connection(search_id)
         except Exception as ex:
@@ -94,7 +94,7 @@ class StixTransmission:
     def is_async(self):
         # Check if the module is async/sync
         try:
-            if self.init_error is not None:
+            if self.init_error:
                 raise self.init_error
             return self.entry_point.is_async()
         except Exception as ex:
