cf915928cc4eff158bb5e797dd46079a5d9ada17
Salman Mesia <salmanmesia@gmail.com>
2021-04-12 11:38:11 -0400

    Added Virus Total STIX Shifter Module and STIX indicator support for STIX 2.1

---
+++ stix_shifter_modules/virus_total/models/constants.py
@@ -0,0 +1,7 @@
+supportedCortexAnalyzerModules: dict = {
+    "VirusTotal_GetReport_3_0": {
+        "path": "virus_total/virustotal.py",
+        "name": "VirusTotalAnalyzer",
+        "service": "get",
+    }
+}
