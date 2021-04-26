4953d6500328430a0cc06d0bc6c3537616de7d57
Md Azam <mdazam@ca.ibm.com>
2021-04-26 14:25:35 -0300

    Update error mapping of QRadar transmission calls
    (cherry picked from commit 738172ac77a534e8aaab563fcbe521260b8d3046)

--- stix_shifter_modules/qradar/stix_transmission/results_connector.py
+++ stix_shifter_modules/qradar/stix_transmission/results_connector.py
@@ -1,11 +1,13 @@
 from stix_shifter_utils.modules.base.stix_transmission.base_results_connector import BaseResultsConnector
 from stix_shifter_utils.utils.error_response import ErrorResponder
+from stix_shifter_utils.utils import logger
 import json
 
 
 class ResultsConnector(BaseResultsConnector):
     def __init__(self, api_client):
         self.api_client = api_client
+        self.logger = logger.set_logger(__name__)
 
     def create_results_connection(self, search_id, offset, length):
         min_range = offset
@@ -17,12 +19,19 @@ class ResultsConnector(BaseResultsConnector):
 
         # Construct a response object
         return_obj = dict()
-        response_dict = json.loads(response.read())
+        error = None
+        response_text = response.read()
+
+        try:
+            response_dict = json.loads(response_text)
+        except ValueError as ex:
+            self.logger.debug(response_text)
+            error = Exception(f'Can not parse response: {ex} : {response_text}')
         
         if 200 <= response_code <= 299:
             return_obj['success'] = True
             return_obj['data'] = response_dict.get('events', response_dict.get('flows'))
         else:
-            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'], error=error)
 
         return return_obj
