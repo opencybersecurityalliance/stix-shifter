40e99d6876f62eecaeb3bbc6661b25d96fbc8da9
Danny Elliott <danny.elliott@ibm.com>
2021-04-29 14:04:45 -0300

    add port to config

--- stix_shifter_modules/mysql/stix_transmission/api_client.py
+++ stix_shifter_modules/mysql/stix_transmission/api_client.py
@@ -13,13 +13,17 @@ class APIClient():
         self.host = connection.get("host")
         self.database = connection.get("database")
         self.table = connection.get("table")
+        self.port = connection.get("port")
+        self.auth_plugin = 'mysql_native_password'
 
     def ping_data_source(self):
         # Pings the data source
         response = {"code": 200, "message": "All Good!"}
-
         try:
-            cnx = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)          
+            cnx = mysql.connector.connect(user=self.user, password=self.password, 
+                                          host=self.host, database=self.database, 
+                                          port=self.port, auth_plugin=self.auth_plugin)  
+
         except mysql.connector.Error as err:
             response["code"] = err.errno
 
@@ -34,11 +38,12 @@ class APIClient():
         return response
 
     def run_search(self, query, start=0, rows=0):
-        # Return the search results. Results must be in JSON format before being translated into STIX 
+        # Return the search results. Results must be in JSON format before translating into STIX 
         response = {"code": 200, "message": "All Good!", "result": []}
 
         try:
-            cnx = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+            cnx = mysql.connector.connect(user=self.user, password=self.password, host=self.host, 
+                                          database=self.database, port=self.port, auth_plugin=self.auth_plugin)
             cursor = cnx.cursor()
             column_query = "SHOW COLUMNS FROM %s" % self.table 
             cursor.execute(column_query)
@@ -47,6 +52,8 @@ class APIClient():
 
             for tuple in column_collection:
                 column_list.append(tuple[0])
+            # Uncomment to see data on newly populated table
+            # query = "select * from {} limit 1".format(self.table)
 
             cursor.execute(query)  
             result_collection = cursor.fetchall()
