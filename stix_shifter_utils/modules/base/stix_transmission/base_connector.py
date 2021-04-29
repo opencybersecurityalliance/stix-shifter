6000b5a38f94f3c0f341b6bc38fedbcd73ff7f30
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2021-04-29 15:11:36 -0400

    + 1

--- stix_shifter_utils/modules/base/stix_transmission/base_connector.py
+++ stix_shifter_utils/modules/base/stix_transmission/base_connector.py
@@ -92,6 +92,6 @@ class BaseConnector:
         result = entry_point.create_results_connection(search_id, offset, length)
         if result.get('success'):
             data = result['data']
-            data = data[:length]
+            data = data[:length + 1]
             result = entry_point.translate_results(data_source, json.dumps(data))
         return result
