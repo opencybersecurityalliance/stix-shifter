c9e6ef952d41bc155eac11443ebda30980c6419a
akshay <akshay.pange@gslab.com>
2021-07-05 17:37:10 +0530

    Handle pagination to get all values more than 50

--- stix_shifter_modules/onelogin/stix_transmission/results_connector.py
+++ stix_shifter_modules/onelogin/stix_transmission/results_connector.py
@@ -4,7 +4,10 @@ from stix_shifter_utils.modules.base.stix_transmission.base_results_connector im
 from stix_shifter_utils.utils.error_response import ErrorResponder
 from stix_shifter_utils.utils import logger
 
+
 class ResultsConnector(BaseResultsConnector):
+    max_limit = 50
+
     def __init__(self, api_client):
         self.init_error = None
         self.api_client = api_client
@@ -16,21 +19,43 @@ class ResultsConnector(BaseResultsConnector):
             self.init_error = self.token_resp
 
     def create_results_connection(self, quary_expr, offset, length):
+        length = int(length)
+        offset = int(offset)
+
+        # total records is the sum of the offset and length(limit) value
+        total_records = offset + length
         try:
-            min_range = offset
-            max_range = offset + length
             if self.init_error:
                 self.logger.error("Token Generation Failed:")
                 return self.init_error
             # Grab the response, extract the response code, and convert it to readable json
-            response_dict = self.api_client.run_search(quary_expr, min_range, max_range, self.access_token)
+            if length <= self.max_limit:
+                # $(offset) param not included as data source not support this
+                response_dict = self.api_client.run_search(quary_expr, total_records, self.access_token)
+            else:
+                response_dict = self.api_client.run_search(quary_expr, self.max_limit, self.access_token)
             response_code = response_dict.code
             # Construct a response object
             response_dict = json.loads(response_dict.read())
+            # print(response_dict)
             return_obj = dict()
             if response_code == 200:
                 return_obj['success'] = True
                 return_obj['data'] = response_dict['data']
+                next_page_link = response_dict['pagination']['next_link']
+                while len(return_obj['data']) < total_records and next_page_link:
+                    try:
+                        response = self.api_client.next_page_run_search(next_page_link)
+                        response_code = response.code
+                        response_dict = json.loads(response.read())
+                        if response_code == 200:
+                            return_obj['data'].extend(response_dict['data'])
+                        else:
+                            ErrorResponder.fill_error(return_obj, response_dict, ['error', 'message'])
+                    except KeyError:
+                        break
+                # slice the records as per the provided offset and length(limit)
+                return_obj['data'] = return_obj['data'][offset:total_records]
             else:
                 ErrorResponder.fill_error(return_obj, response_dict, ['message'])
             return return_obj
