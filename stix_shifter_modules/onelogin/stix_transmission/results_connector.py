9df59569701bf45cf67d20b3d26ecd0eca93225f
akshay <akshay.pange@gslab.com>
2021-07-12 18:12:09 +0530

    Added custom generic filter and new mappings

--- stix_shifter_modules/onelogin/stix_transmission/results_connector.py
+++ stix_shifter_modules/onelogin/stix_transmission/results_connector.py
@@ -28,6 +28,8 @@ class ResultsConnector(BaseResultsConnector):
             if self.init_error:
                 self.logger.error("Token Generation Failed:")
                 return self.init_error
+            # Separate out api supported url params
+            quary_expr, filter_attr = ResultsConnector.modify_query_expr(quary_expr)
             # Grab the response, extract the response code, and convert it to readable json
             if length <= self.max_limit:
                 # $(offset) param not included as data source not support this
@@ -37,25 +39,33 @@ class ResultsConnector(BaseResultsConnector):
             response_code = response_dict.code
             # Construct a response object
             response_dict = json.loads(response_dict.read())
-            # print(response_dict)
             return_obj = dict()
             if response_code == 200:
                 return_obj['success'] = True
                 return_obj['data'] = response_dict['data']
                 next_page_link = response_dict['pagination']['next_link']
+                filter_flag = True
                 while len(return_obj['data']) < total_records and next_page_link:
                     try:
                         response = self.api_client.next_page_run_search(next_page_link)
                         response_code = response.code
                         response_dict = json.loads(response.read())
                         if response_code == 200:
-                            return_obj['data'].extend(response_dict['data'])
+                            if filter_attr:
+                                filter_flag = False
+                                # filter data based on filter-attributes
+                                response_dict = ResultsConnector.filter_response(response_dict, filter_attr)
+                                return_obj['data'].extend(response_dict['data'])
                         else:
                             ErrorResponder.fill_error(return_obj, response_dict, ['error', 'message'])
                     except KeyError:
                         break
+                # filter data if not filtered in above while loop
+                if filter_flag:
+                    return_obj = ResultsConnector.filter_response(return_obj, filter_attr)
                 # slice the records as per the provided offset and length(limit)
                 return_obj['data'] = return_obj['data'][offset:total_records]
+
             else:
                 ErrorResponder.fill_error(return_obj, response_dict, ['message'])
             return return_obj
@@ -81,7 +91,28 @@ class ResultsConnector(BaseResultsConnector):
             self.logger.error('error while generating access token: {}'.format(e))
         return return_obj
 
+    @staticmethod
+    def modify_query_expr(quary_expr):
+        valid_filter_attributes = ["client_id", "directory_id", "created_at", "id", "until", "event_type_id", "limit",
+                                  "since", "resolution", "user_id"]
+        api_quary_attr = []
+        filter_attr = []
+        quary_expr_list = quary_expr.split("&")
+        for attribute in quary_expr_list:
+            if attribute.split("=")[0] in valid_filter_attributes:
+                api_quary_attr.append(attribute)
+            else:
+                filter_attr.append(attribute)
+        quary_expr = '&'.join(api_quary_attr)
+        return quary_expr, filter_attr
 
-
-
-
+    @staticmethod
+    def filter_response(response_dict, filter_attr):
+        try:
+            for attr in filter_attr:
+                response_dict['data'] = list(
+                    filter(lambda person: person[attr.split("=")[0]] == attr.split("=")[1],
+                           response_dict['data']))
+        except KeyError as ex:
+            raise KeyError(f"Invalid parameter {ex}")
+        return response_dict
