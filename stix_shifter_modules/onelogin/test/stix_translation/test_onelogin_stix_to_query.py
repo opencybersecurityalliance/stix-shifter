fe9697e2ec9c333d899dcfd8aa0ec1f8412c93a8
akshay <akshay.pange@gslab.com>
2021-07-15 17:49:09 +0530

    Resolved errors and warning in validation of STIX bundles

--- stix_shifter_modules/onelogin/test/stix_translation/test_onelogin_stix_to_query.py
+++ stix_shifter_modules/onelogin/test/stix_translation/test_onelogin_stix_to_query.py
@@ -24,25 +24,25 @@ class TestStixToQuery(unittest.TestCase, object):
         _test_query_assertions(query, queries)
 
     def test_account_id_query(self):
-        stix_pattern = "[user-account:account_id = '12345678']"
+        stix_pattern = "[x-onelogin-user-account:account_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'account_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_actor_user_id_query(self):
-        stix_pattern = "[user-account:actor_user_id = '12345678']"
+        stix_pattern = "[x-onelogin-user-account:actor_user_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'actor_user_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_assuming_acting_user_id_query(self):
-        stix_pattern = "[user-account:assuming_acting_user_id = '12345678']"
+        stix_pattern = "[x-onelogin-user-account:assuming_acting_user_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'assuming_acting_user_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_actor_user_name_query(self):
-        stix_pattern = "[user-account:actor_user_name = 'Jon']"
+        stix_pattern = "[x-onelogin-user-account:actor_user_name = 'Jon']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'actor_user_name=Jon&limit=50'
         _test_query_assertions(query, queries)
@@ -54,61 +54,61 @@ class TestStixToQuery(unittest.TestCase, object):
         _test_query_assertions(query, queries)
 
     def test_client_id_query(self):
-        stix_pattern = "[x-onelogin:client_id = '12345678']"
+        stix_pattern = "[x-onelogin-finding:client_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'client_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_directory_id_query(self):
-        stix_pattern = "[x-onelogin:directory_id = '12345678']"
+        stix_pattern = "[x-onelogin-finding:directory_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'directory_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_event_type_id_query(self):
-        stix_pattern = "[x-onelogin:event_type_id = '12345678']"
+        stix_pattern = "[x-onelogin-finding:event_type_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'event_type_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_id_query(self):
-        stix_pattern = "[x-onelogin:id = '12345678']"
+        stix_pattern = "[x-onelogin-finding:unique_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_resolution_query(self):
-        stix_pattern = "[x-onelogin:resolution = '12345678']"
+        stix_pattern = "[x-onelogin-finding:resolution = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'resolution=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_since_query(self):
-        stix_pattern = "[x-onelogin:since = '2021-06-22T13:12:06.437Z']"
+        stix_pattern = "[x-onelogin-finding:since = '2021-06-22T13:12:06.437Z']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'since=2021-06-22T13:12:06.437Z&limit=50'
         _test_query_assertions(query, queries)
 
     def test_until_query(self):
-        stix_pattern = "[x-onelogin:until = '2021-06-22T13:12:06.437Z']"
+        stix_pattern = "[x-onelogin-finding:until = '2021-06-22T13:12:06.437Z']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'until=2021-06-22T13:12:06.437Z&limit=50'
         _test_query_assertions(query, queries)
 
     def test_proxy_ip_query(self):
-        stix_pattern = "[x-onelogin:proxy_ip = '127.0.0.1']"
+        stix_pattern = "[x-onelogin-finding:proxy_ip = '127.0.0.1']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'proxy_ip=127.0.0.1&limit=50'
         _test_query_assertions(query, queries)
 
     def test_policy_id_query(self):
-        stix_pattern = "[x-onelogin:policy_id = '12345678']"
+        stix_pattern = "[x-onelogin-finding:policy_id = '12345678']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'policy_id=12345678&limit=50'
         _test_query_assertions(query, queries)
 
     def test_policy_name_query(self):
-        stix_pattern = "[x-onelogin:policy_name = 'Default']"
+        stix_pattern = "[x-onelogin-finding:policy_name = 'Default']"
         query = translation.translate('onelogin', 'query', '{}', stix_pattern)
         queries = 'policy_name=Default&limit=50'
         _test_query_assertions(query, queries)
