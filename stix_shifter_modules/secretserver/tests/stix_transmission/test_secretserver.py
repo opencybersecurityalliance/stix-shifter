e7c8b1ef03292d463f35ae9ab36707c27461abc7
Deepshikha8514 <45089286+Deepshikha8514@users.noreply.github.com>
2021-07-28 02:08:19 +0530

    Update test_secretserver.py

--- stix_shifter_modules/secretserver/tests/stix_transmission/test_secretserver.py
+++ stix_shifter_modules/secretserver/tests/stix_transmission/test_secretserver.py
@@ -1,6 +1,6 @@
 
 from stix_shifter.stix_transmission import stix_transmission
-from stix_shifter_modules.async_dummy.entry_point import EntryPoint
+from stix_shifter_modules.secretserver.entry_point import EntryPoint
 from stix_shifter_utils.modules.base.stix_transmission.base_status_connector import Status
 import unittest
 from unittest.mock import patch
@@ -31,7 +31,7 @@ class TestSecretServerConnection(unittest.TestCase, object):
         }
         check_async = entry_point.is_async()
 
-        assert  check_async
+        assert not check_async
 
     @patch('stix_shifter_modules.secretserver.stix_transmission.api_client.APIClient.create_search')
     def test_query_response(self, mock_query_response, mock_api_client):
@@ -72,7 +72,7 @@ class TestSecretServerConnection(unittest.TestCase, object):
         }
         connection = {
 
-            "host": "secretserver.ibm.com",
+            "host": "secretserver.com",
 
         }
 
@@ -85,3 +85,42 @@ class TestSecretServerConnection(unittest.TestCase, object):
         assert 'status' in status_response
         assert status_response['status'] == Status.COMPLETED.value
 
+    @patch('stix_shifter_modules.secretserver.stix_transmission.api_client.APIClient.get_search_results', autospec=True)
+    def test_results_response(self, mock_results_response, mock_api_client):
+        mock_api_client.return_value = None
+        mocked_return_value = """{
+               "success": true, 
+               "data": [
+                   "ItemId": 1,
+                   "SecretName":"abc,
+                   "EventTime":"2021-07-06T02:56:58.413",
+                   "EventSubject" : "[[SecretServer]] [Secret] MySql_130 [Check Out] by New",
+                   "IpAddress": "127.0.0.1",
+                    "username": "New",
+                    "Server": "9.202.181.130"
+                       
+                   ]
+           }"""
+        mock_results_response.return_value = SecretServerMockResponse(200, mocked_return_value)
+
+        config = {
+            "auth": {
+                "username": "admin",
+                "password": "password123"
+            }
+        }
+        connection = {
+
+            "host": "secretserver.com",
+
+        }
+
+        search_id = "eyJxdWVyeSI6ICJTRUxFQ1QgKiBGUk9NIFNlY3JldEV2ZW50RGV0YWlsX25ldyBXSEVSRSBFdmVudFN1YmplY3QgTElLRSAnJSUlJyBTVEFSVCB0JzIwMTktMDEtMjhUMTI6MjQ6MDEuMDA5WicgU1RPUCB0JzIwMjEtMDctMTRUMTI6NTQ6MDEuMDA5WiciLCAidGFyZ2V0IiA6ICJodHRwOi8vOS40Ni44Ni4xMjAvU2VjcmV0U2VydmVyL29hdXRoMi90b2tlbiJ9"
+        offset = 0
+        length = 1
+        transmission = stix_transmission.StixTransmission('secretserver', connection, config)
+        results_response = transmission.results(search_id, offset, length)
+
+        assert results_response is not None
+        assert results_response['success']
+        assert 'data' in results_response
