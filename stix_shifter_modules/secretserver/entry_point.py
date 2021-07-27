4de5d9cf61113848779340d6e7866a9871f21b74
Deepshikha8514 <45089286+Deepshikha8514@users.noreply.github.com>
2021-07-28 02:04:39 +0530

    Update entry_point.py

--- stix_shifter_modules/secretserver/entry_point.py
+++ stix_shifter_modules/secretserver/entry_point.py
@@ -12,19 +12,10 @@ import os
 
 class EntryPoint(BaseEntryPoint):
 
-    # python main.py translate async_dummy query '{}' "[ipv4-addr:value = '127.0.0.1']"
-    # python main.py translate async_dummy:dialect1 query '{}' "[ipv4-addr:value = '127.0.0.1']"
-    # python main.py translate async_dummy:dialect2 query '{}' "[ipv4-addr:value = '127.0.0.1']"
-
     def __init__(self, connection={}, configuration={}, options={}):
         super().__init__(connection, configuration, options)
+        self.set_async(False)
         if connection:
-
-            # Use default transmission setup otherwise...
-            # self.setup_transmission_simple(connection, configuration)
-
-            # ...implement your own setup similar to the following:
-
             api_client = APIClient(connection, configuration)
             ping_connector = PingConnector(api_client)
             query_connector = QueryConnector(api_client)
@@ -37,16 +28,12 @@ class EntryPoint(BaseEntryPoint):
             self.set_status_connector(status_connector)
             self.set_results_connector(results_connector)
             self.set_delete_connector(delete_connector)
-        
-        # Use default translation setup with default dialect otherwise...
-        # self.setup_translation_simple(dialect_default='default')
-        # ...implement your own setup similar to the following:
 
         basepath = os.path.dirname(__file__)
         filepath = os.path.abspath(
             os.path.join(basepath, "stix_translation"))
 
-        dialect = 'dialect1'
+        dialect = 'event'
         query_translator = QueryTranslator(options, dialect, filepath)
         results_translator = ResultsTranslator(options, dialect, filepath)
         self.add_dialect(dialect, query_translator=query_translator, results_translator=results_translator, default=True)
