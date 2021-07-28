83d0fcb5da9595935157d80cdb4435416289c2eb
Yurii Klymenko <Yurii.Klymenko@ibm.com>
2021-07-28 17:26:53 -0400

    enable debug

--- stix_shifter_utils/utils/error_response.py
+++ stix_shifter_utils/utils/error_response.py
@@ -85,7 +85,7 @@ class ErrorResponder():
         if error is not None:
             str_error = str(error)
             logger.error("error occurred: " + str_error)
-            logger.debug(utils_logger.exception_to_string(error))
+            logger.error(utils_logger.exception_to_string(error))
             if isinstance(error, SSLError):
                 error_code = ErrorCode.TRANSMISSION_AUTH_SSL
                 error_msg = 'Wrong certificate: ' + str_error
