f865d0b782a1d2e6433dfebe975efad6dcb65d20
Yurii Klymenko <11822597+yurii-klymenko@users.noreply.github.com>
2021-08-12 08:42:31 -0400

    Qradar perf test connector (#668)
    * src raw events 500
    
    * enable debug
    
    * events 5000 + 5001
    
    * qradar_test_perf connector
    
    * revert debug logging

--- stix_shifter_utils/modules/base/stix_transmission/base_results_connector.py
+++ stix_shifter_utils/modules/base/stix_transmission/base_results_connector.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import json
+import time
 
 
 class BaseResultsConnector(object, metaclass=ABCMeta):
@@ -23,9 +24,13 @@ class BaseResultsConnector(object, metaclass=ABCMeta):
         raise NotImplementedError()
 
     def create_results_stix_connection(self, entry_point, search_id, offset, length, data_source):
+        stats = []
         result = entry_point.create_results_connection(search_id, offset, length)
+        stats.append({'action': 'transmission', 'time': int(time.time()*1000)})
         if result.get('success'):
             data = result['data']
             data = data[:int(length)]
             result = entry_point.translate_results(data_source, json.dumps(data))
+            stats.append({'action': 'translation', 'time': int(time.time()*1000)})
+        result['stats'] = stats
         return result
