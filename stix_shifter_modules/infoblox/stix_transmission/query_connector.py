9d77cedcaffc6202353e0b62457c2a8c4daf05e0
Ryan Brown <ryanbrown.dev@gmail.com>
2021-09-01 07:10:02 -0700

    Adding Infoblox Stix Shifter Module (#671)

---
+++ stix_shifter_modules/infoblox/stix_transmission/query_connector.py
@@ -0,0 +1,9 @@
+from stix_shifter_utils.modules.base.stix_transmission.base_connector import BaseQueryConnector
+
+
+class QueryConnector(BaseQueryConnector):
+    def __init__(self, api_client):
+        self.api_client = api_client
+
+    def create_query_connection(self, query):
+        return {"success": True, "search_id": query}
