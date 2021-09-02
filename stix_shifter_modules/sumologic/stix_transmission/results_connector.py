e2089b3e68c0433115cbd0d90f59ac0c73971618
Ismail Memon <ismail.memon@gslab.com>
2021-09-02 15:49:17 +0530

    Initial changes for sumologic connector. Added proposed mappings

---
+++ stix_shifter_modules/sumologic/stix_transmission/results_connector.py
@@ -0,0 +1,28 @@
+from stix_shifter_utils.modules.base.stix_transmission.base_results_connector import BaseResultsConnector
+from stix_shifter_utils.utils.error_response import ErrorResponder
+from stix_shifter_utils.utils import logger
+
+class ResultsConnector(BaseResultsConnector):
+    def __init__(self, api_client):
+        self.api_client = api_client
+        self.logger = logger.set_logger(__name__)
+
+    def create_results_connection(self, search_id, offset, length):
+        try:
+            min_range = offset
+            max_range = offset + length
+            # Grab the response, extract the response code, and convert it to readable json
+            response_dict = self.api_client.get_search_results(search_id, min_range, max_range)
+            response_code = response_dict["code"]
+
+            # Construct a response object
+            return_obj = dict()
+            if response_code == 200:
+                return_obj['success'] = True
+                return_obj['data'] = response_dict['data']
+            else:
+                ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+            return return_obj
+        except Exception as err:
+            self.logger.error('error when getting search results: {}'.format(err))
+            raise
