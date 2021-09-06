4689968d746c3fbb552bc2e5cb8b0f82e7e5a042
Ismail Memon <ismail.memon@gslab.com>
2021-09-06 13:17:05 +0530

    Initialized sumologic to use in other functions.

--- stix_shifter_modules/sumologic/stix_transmission/api_client.py
+++ stix_shifter_modules/sumologic/stix_transmission/api_client.py
@@ -1,25 +1,34 @@
+import time
 from sumologic import SumoLogic
 
 
 class APIClient():
 
     def __init__(self, connection, configuration):
-       self.client = "https://api.%s.sumologic.com/api" % (connection.get("region"))
-       self.auth = configuration.get('auth')
+        self.endpoint = "https://api.%s.sumologic.com/api" % (connection.get("region"))
+        self.auth = configuration.get('auth')
+        self.client = SumoLogic(self.auth.get("username"), self.auth.get("password"), endpoint=self.endpoint)
 
     def ping_data_source(self):
         # Pings the data source
-        sumo = SumoLogic(self.auth.get("username"), self.auth.get("password"), endpoint=self.client)
-        sumo.collectors()
+        self.client.collectors()
         return {"code": 200, "success": True}
 
     def create_search(self, query_expression):
         # Queries the data source
+        self.search_job = None
         return {"code": 200, "query_id": "uuid_1234567890"}
 
     def get_search_status(self, search_id):
         # Check the current status of the search
-        return {"code": 200, "status": "COMPLETED"}
+        print(search_id)
+        status = self.client.search_job_status(self.search_job)
+        while status['state'] != 'DONE GATHERING RESULTS':
+            if status['state'] == 'CANCELLED':
+                break
+            time.sleep(5)
+            status = self.client.search_job_status(self.search_job)
+        return {"code": 200, "status": status['state']}
 
     def get_search_results(self, search_id, range_start=None, range_end=None):
         # Return the search results. Results must be in JSON format before being translated into STIX
