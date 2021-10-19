8db357b0257e3d201037147a90a8d24ebe0d94e1
shahvs <vatsal.shah@gslab.com>
2021-10-19 21:38:18 +0530

    Resolved testing error

--- stix_shifter_modules/datadog/test/stix_translation/test_datadog_stix_to_query.py
+++ stix_shifter_modules/datadog/test/stix_translation/test_datadog_stix_to_query.py
@@ -17,125 +17,125 @@ class TestStixToQuery(unittest.TestCase, object):
     def test_ipv4_query(self, mock_time):
         stix_pattern = "[x-oca-event:code = 12345678 OR domain-name:value = 'abc.com']"
         query = translation.translate('datadog', 'query', 'datadog', stix_pattern)
-        queries = ['{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "events"}',
+        queries = ['{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "events"}',
                    '{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "events"}',
-                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "events"}']
+                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "processes"}',
+                   '{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}',
+                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "processes"}']
         assert query['queries'] == queries
 
     @patch('time.time', return_value=12345678)
     def test_domain_name_query(self, mock_time):
         stix_pattern = "[domain-name:value = 'abc.com']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"host": "abc.com", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_event_id_query(self, mock_time):
         stix_pattern = "[x-oca-event:code = 12345678]"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "events"}',
-                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}',
+                   '{"query": {"id_str": "12345678", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_priority_query(self, mock_time):
         stix_pattern = "[x-datadog-event:priority = 'info']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"priority": "info", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"priority": "info", "start": 12345378, "end": 12345678}, "source": "events"}',]
+        queries = ['{"query": {"priority": "info", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"priority": "info", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_title_query(self, mock_time):
         stix_pattern = "[x-oca-event:outcome = 'title']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"title": "title", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"title": "title", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"title": "title", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"title": "title", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_text_query(self, mock_time):
         stix_pattern = "[artifact:payload_bin = 'text']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"text": "text", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"text": "text", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"text": "text", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"text": "text", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_device_name_query(self, mock_time):
         stix_pattern = "[x-oca-event:agent = 'Windows']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"device_name": "Windows", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"device_name": "Windows", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"device_name": "Windows", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"device_name": "Windows", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_source_query(self, mock_time):
         stix_pattern = "[x-oca-event:module = 'source' AND x-oca-event:module = 'default']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"source": "default,source", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"source": "default,source", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"source": "default,source", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"source": "default,source", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_monitor_id_query(self, mock_time):
         stix_pattern = "[x-datadog-event:monitor_id = '12345678']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"monitor_id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"monitor_id": 12345678, "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"monitor_id": 12345678, "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"monitor_id": 12345678, "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_tags_query(self, mock_time):
         stix_pattern = "[x-datadog-event:tags = 'tags']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_is_aggregate_query(self, mock_time):
         stix_pattern = "[x-datadog-event:is_aggregate = 'true']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"unaggregated": "true", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"unaggregated": "true", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"unaggregated": "true", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"unaggregated": "true", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_alert_type_query(self, mock_time):
         stix_pattern = "[x-datadog-event:alert_type = 'alert_type']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_start_query(self, mock_time):
         stix_pattern = "[x-ibm-finding:start = '2021-01-28T12:24:01.009Z']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"start": 1611836641, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"start": 1611836641, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"start": 1611836641, "end": 12345678}, "source": "events"}',
+                   '{"query": {"start": 1611836641, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     def test_end_query(self):
         stix_pattern = "[x-ibm-finding:end = '2021-01-28T12:24:01.009Z']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"end": 1611836641, "start": 1611836341}, "source": "processes"}',
-                   '{"query": {"end": 1611836641, "start": 1611836341}, "source": "events"}']
+        queries = ['{"query": {"end": 1611836641, "start": 1611836341}, "source": "events"}',
+                   '{"query": {"end": 1611836641, "start": 1611836341}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_time_observed_query(self, mock_time):
         stix_pattern = "[x-ibm-finding:time_observed = '2021-01-28T12:24:01.009Z']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"date_happened": 1611836641, "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"date_happened": 1611836641, "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"date_happened": 1611836641, "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"date_happened": 1611836641, "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
@@ -163,32 +163,32 @@ class TestStixToQuery(unittest.TestCase, object):
     def test_query_from_multiple_observation_expressions_joined_by_AND(self, mock_time):
         stix_pattern = "[x-datadog-event:alert_type = 'alert_type'] AND [x-datadog-event:tags = 'tags']"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "events"}',
-                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"alert_type": "alert_type", "start": 12345378, "end": 12345678}, "source": "processes"}',
+                   '{"query": {"tags": "tags", "start": 12345378, "end": 12345678}, "source": "processes"}']
         assert query['queries'][0] == queries[0]
 
     def test_query_comparator_operator_AND_with_same_field(self):
         stix_pattern = "[domain-name:value = 'abc.com'] START t'2021-01-28T12:24:01.009Z' STOP t'2021-07-25T12:54:01.009Z'"
         query = translation.translate('datadog', 'query', 'datadog', stix_pattern)
-        queries = ['{"query": {"host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "processes"}',
-                   '{"query": {"host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "events"}']
+        queries = ['{"query": {"host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "events"}',
+                   '{"query": {"host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     def test_query_comparator_operator_AND_with_different_field(self):
         stix_pattern = "[domain-name:value = 'abc.com' AND x-datadog-event:alert_type = 'alert_type'] START t'2021-01-28T12:24:01.009Z' STOP t'2021-07-25T12:54:01.009Z'"
         query = translation.translate('datadog', 'query', 'datadog', stix_pattern)
-        queries = ['{"query": {"alert_type": "alert_type", "host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "processes"}',
-                   '{"query": {"alert_type": "alert_type", "host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "events"}']
+        queries = ['{"query": {"alert_type": "alert_type", "host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "events"}',
+                   '{"query": {"alert_type": "alert_type", "host": "abc.com", "start": 1611836641, "end": 1627217641}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     @patch('time.time', return_value=12345678)
     def test_IN_query(self, mock_time):
         stix_pattern = "[x-datadog-event:alert_type IN ('alert_type', 'default')]"
         query = translation.translate('datadog', 'query', '{}', stix_pattern)
-        queries = ['{"query": {"alert_type": ["alert_type", "default"], "start": 12345378, "end": 12345678}, "source": "processes"}',
-                   '{"query": {"alert_type": ["alert_type", "default"], "start": 12345378, "end": 12345678}, "source": "events"}']
+        queries = ['{"query": {"alert_type": ["alert_type", "default"], "start": 12345378, "end": 12345678}, "source": "events"}',
+                   '{"query": {"alert_type": ["alert_type", "default"], "start": 12345378, "end": 12345678}, "source": "processes"}']
         _test_query_assertions(query, queries)
 
     def test_invalid_stix_pattern(self):
