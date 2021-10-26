80c98053dcb6bd7098377dccc07ce08e37b3e7c5
jyoti.wakchaure <jyoti.wakchaure@hcl.com>
2021-10-26 19:34:00 +0530

    Removed credentials

--- stix_shifter_modules/proofpoint/tests/stix_transmission/test_proofpoint.py
+++ stix_shifter_modules/proofpoint/tests/stix_transmission/test_proofpoint.py
@@ -7,15 +7,15 @@ class TestProofpointConnection(unittest.TestCase, object):
 
     def connection(self):
         return {
-            "host": "tap-api-v2.proofpoint.com",
+            "host": "host",
             "port": 8080,
         }
 
     def configuration(self):
         return {
             "auth": {
-                "principal": "96dbfb1e-e6bf-f298-3080-3cab5b1cfe19",
-                "secret": "1ee4776401dc372e608cb9ae4810cec5420c2689ff38befa1ac46bd17da20904"
+                "principal": "principal",
+                "secret": "secret"
             }
         }
 
