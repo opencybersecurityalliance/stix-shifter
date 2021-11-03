ea88725c706ce63bf5fc21b518c4e7d81e5080ef
Arthur Muradyan <arthur.muradyan@ibm.com>
2021-11-03 12:14:34 -0300

    Accept incoming argument value for namespace in stix-shifter build sc… (#717)

--- deployment/ibm_cloud_pak_for_security/_build.sh
+++ deployment/ibm_cloud_pak_for_security/_build.sh
@@ -11,6 +11,7 @@ validate_cmd () {
 }
 
 IMAGE_URL="$1"
+NAMESPACE="$2"
 
 validate_cmd openssl
 validate_cmd python3
@@ -28,7 +29,12 @@ validate_cmd kubectl
 validate_cmd oc
 
 FILE_PREFIX=stix_shifter_modules_
-NAMESPACE=cp4s
+if [ "X$NAMESPACE" == "X" ]; then 
+    NAMESPACE=`kubectl config view --minify --output 'jsonpath={..namespace}' | awk '{print $1}'`
+fi 
+if [ "${IMAGE_URL}" == "no-repository" ] ; then
+   IMAGE_URL=""
+fi
 TIMESTAMP=`date '+%Y%m%d%H%M%S'`
 
 if [ ! -z "${IMAGE_URL}" ]; then
