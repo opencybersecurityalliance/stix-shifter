3061195568dd995c7a966f4b4a060d22406c5774
Arthur Muradyan <arthur.muradyan@ibm.com>
2022-01-21 13:56:08 -0500

    Removed test files

--- test3.py
+++
@@ -1,6 +0,0 @@
-from stix_shifter.stix_translation import stix_translation
-
-translation = stix_translation.StixTranslation()
-response = translation.translate('qradar', 'query', '{}', "[ipv4-addr:value = '127.0.0.1']", {})
-
-print(response)
