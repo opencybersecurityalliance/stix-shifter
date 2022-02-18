35f5cd8f6e142edebf5b1069cf0fad4a568659ad
RMS <kr.riteshsinha@gmail.com>
2022-02-18 18:42:47 +0530

    added few more mapping

--- stix_shifter_modules/verify/stix_translation/transformers.py
+++ stix_shifter_modules/verify/stix_translation/transformers.py
@@ -1,8 +1,8 @@
 from stix_shifter_utils.stix_translation.src.utils.transformers import ValueTransformer
 
 class VerifyStaticTransformer(ValueTransformer):
-    """A value transformer that always returns the string 'IBM Security Verify IAM'"""
+    """A value transformer that always returns the string 'IBM Security Verify Event'"""
 
     @staticmethod
     def transform(value):
-        return "IBM Security Verify IAM"
+        return "IBM Security Verify Event"
