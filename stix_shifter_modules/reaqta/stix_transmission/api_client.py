3354de8708bfcbec945f9d89a6aa9229444f83de
Md Azam <mdazam@ca.ibm.com>
2022-03-28 17:04:17 -0300

    ping connecton

--- stix_shifter_modules/reaqta/stix_transmission/api_client.py
+++ stix_shifter_modules/reaqta/stix_transmission/api_client.py
@@ -1,18 +1,20 @@
 from stix_shifter_utils.stix_transmission.utils.RestApiClient import RestApiClient
 import requests
+import json
 
 class APIClient():
-    AUTH_ENDPOINT = "/rqt-api/1/authenticate"
-    EVENT_ENDPOINT = "/rqt-api/1/events/hunt"
+    AUTH_ENDPOINT = "rqt-api/1/authenticate"
+    EVENT_ENDPOINT = "rqt-api/1/events/hunt"
 
     def __init__(self, connection, configuration):
-        headers = dict()
+        self.headers = dict()
+        self.headers['Accept'] = 'application/json'
         url_modifier_function = None
         auth = configuration.get('auth')
         self.host = connection.get('host')
         self.client = RestApiClient(connection.get('host'),
-                                    connection.get('port', None),
-                                    headers,
+                                    None,
+                                    self.headers,
                                     url_modifier_function=url_modifier_function,
                                     cert_verify=connection.get('selfSignedCert', True),
                                     sni=connection.get('sni', None)
@@ -23,16 +25,53 @@ class APIClient():
 
     def ping_data_source(self):
         # Pings the data source
-        auth_host = 'https://' + self.host + self.AUTH_ENDPOINT
-        auth_data = {'id' : self.app_id, 'secret' : self.secret_key}
-        return requests.post(auth_host, data=auth_data)
-        # return self.client.call_api(auth_host, 'POST', data=auth_data, timeout=self.timeout)
+        # return requests.post(auth_host, data=auth_data)
+        # return self.client.call_api(self.AUTH_ENDPOINT, 'POST', data=auth_data)
+        return self.get_token()
 
     def get_search_results(self, search_id, range_start=None, range_end=None):
         # Return the search results. Results must be in JSON format before being translated into STIX
-        return {"code": 200, "data": "Results from search"}
 
-    def delete_search(self, search_id):
-        # Optional since this may not be supported by the data source API
-        # Delete the search
-        return {"code": 200, "success": True}
+        params = dict()
+        token_response = self.get_token()
+        response_code = token_response['code']
+        if response_code == 200:
+            self.headers['Authorization'] = 'Bearer {}'.format(token_response['token'])
+        
+        # LOOKS LIKE MAX COUNT is 500. response doesn't show why it fails
+        params['count'] = int(range_end) - int(range_start)
+        
+        #$ip="192.168.122.83" OR $ip="192.168.122.84" AND happenedAfter="yyyy-MM-ddTHH:mm:ss.SSSZ" AND happenedBefore="yyyy-MM-ddTHH:mm:ss.SSSZ"
+        # search_id = '$ip="172.16.60.184" and hasAlert=t'
+        body_data = {'query': search_id}
+        
+        return self.client.call_api(self.EVENT_ENDPOINT, 'POST', urldata=params, headers=self.headers, data=body_data)
+        events=json.loads(response.response.text)
+        
+        print(json.dumps(events, indent=4))
+
+        # return {"code": 200, "data": "Results from search"}
+    
+    def get_token(self):
+        auth_data = dict()
+        response_dict= dict()
+        auth_data['id'] = self.app_id
+        auth_data['secret'] = self.secret_key
+        # auth_data = {'id' : self.app_id, 'secret' : self.secret_key}
+        try:
+            response = self.client.call_api(self.AUTH_ENDPOINT, 'POST', headers=self.headers, data=auth_data)
+            
+            response_dict['code'] = response.code
+            response_text = json.loads(response.response.text)
+            if response.code == 200:
+                response_dict['token'] = response_text['token']
+            else:
+                response_dict['message'] = response_text
+        except Exception as ex:
+            if ex.__class__.__name__ is 'ConnectionError':
+                raise ConnectionError(ex)
+            else:
+                response_dict['type'] = ex.__class__.__name__
+                response_dict['message'] = ex
+
+        return response_dict
--- stix_shifter_modules/reaqta/stix_transmission/connector.py
+++ stix_shifter_modules/reaqta/stix_transmission/connector.py
@@ -12,18 +12,52 @@ class Connector(BaseSyncConnector):
         self.connector = __name__.split('.')[1]
     
     def ping_connection(self):
+        response_dict = dict()
         try:
             response = self.api_client.ping_data_source()
-            response_code = response.status_code
+            response_code = response['code']
             
             return_obj = dict()
-            if response_code == 200            :
+            if response_code == 200:
                 return_obj['success'] = True
+            elif response_code == 401:
+                response_dict['type'] = 'AuthenticationError'
+                response_dict['message'] = 'Invalid App Secret key. API Response: {}'.format(response['message'])
+                ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
             else:
-                response_text = response.bytes
-                response_dict = response_text.decode('utf-8')
+                response_dict['type'] = 'AuthenticationError'
+                response_dict['message'] = 'Invalid Authentication: Verify App ID. API Response: {}'.format(response['message'])
                 ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
             return return_obj
         except Exception as err:
             self.logger.error('error when pinging datasource {}:'.format(err))
+            raise
+    
+    def create_results_connection(self, search_id, offset, length):
+        try:
+            min_range = offset
+            max_range = offset + length
+            # Grab the response, extract the response code, and convert it to readable json
+            response = self.api_client.get_search_results(search_id, min_range, max_range)
+            response_code = response.code
+            response_text = response.read()
+            try:
+                response_dict = json.loads(response_text)
+            except ValueError as ex:
+                self.logger.debug(response_text)
+                error = Exception(f'Can not parse response from reaqta. The response is not a valid json: {response_text} : {ex}')
+            
+            # # Construct a response object
+            return_obj = dict()
+            if response_code == 200:
+                return_obj['success'] = True
+                return_obj['data'] = response_dict['result']
+            else:
+                response_string = response_text.decode()
+                ErrorResponder.fill_error(return_obj, response_string, ['message'], connector=self.connector)
+            return return_obj
+        except Exception as err:
+            self.logger.error('error when getting search results: {}'.format(err))
+            import traceback
+            self.logger.error(traceback.print_stack())
             raise
