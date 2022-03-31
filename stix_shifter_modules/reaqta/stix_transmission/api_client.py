c7c7e97a6a9f1579809472af545d5d8681b58a1c
Md Azam <mdazam@ca.ibm.com>
2022-03-31 15:07:26 -0300

    update transmission module

--- stix_shifter_modules/reaqta/stix_transmission/api_client.py
+++ stix_shifter_modules/reaqta/stix_transmission/api_client.py
@@ -1,5 +1,6 @@
 from stix_shifter_utils.stix_transmission.utils.RestApiClient import RestApiClient
 import requests
+from datetime import datetime, timedelta
 import json
 
 class APIClient():
@@ -22,56 +23,71 @@ class APIClient():
         self.timeout = connection['options'].get('timeout')
         self.app_id = auth['app_id']
         self.secret_key = auth['secret_key']
+        self.token = None
+        self.token_expiresat = None
 
     def ping_data_source(self):
         # Pings the data source
-        # return requests.post(auth_host, data=auth_data)
-        # return self.client.call_api(self.AUTH_ENDPOINT, 'POST', data=auth_data)
         return self.get_token()
 
-    def get_search_results(self, search_id, range_start=None, range_end=None):
+    def get_search_results(self, search_id, length):
         # Return the search results. Results must be in JSON format before being translated into STIX
-
         params = dict()
+        params['count'] = length
         token_response = self.get_token()
         response_code = token_response['code']
         if response_code == 200:
             self.headers['Authorization'] = 'Bearer {}'.format(token_response['token'])
-        
-        # LOOKS LIKE MAX COUNT is 500. response doesn't show why it fails
-        params['count'] = int(range_end) - int(range_start)
-        
-        #$ip="192.168.122.83" OR $ip="192.168.122.84" AND happenedAfter="yyyy-MM-ddTHH:mm:ss.SSSZ" AND happenedBefore="yyyy-MM-ddTHH:mm:ss.SSSZ"
-        # search_id = '$ip="172.16.60.184" and hasAlert=t'
+        else:
+            raise Exception(token_response)
+
         body_data = {'query': search_id}
         
         return self.client.call_api(self.EVENT_ENDPOINT, 'POST', urldata=params, headers=self.headers, data=body_data)
-        events=json.loads(response.response.text)
-        
-        print(json.dumps(events, indent=4))
-
-        # return {"code": 200, "data": "Results from search"}
     
+    def page_search(self, search_id, next_page_url, length):
+        params = dict()
+        params['count'] = length
+        if not self.token_expired():
+            token_response = self.get_token()
+            response_code = token_response['code']
+            if response_code == 200:
+                self.headers['Authorization'] = 'Bearer {}'.format(token_response['token'])
+        
+        body_data = {'query': search_id}
+        page = next_page_url.split('?', maxsplit=1)[1]
+        next_page_endpoint = self.EVENT_ENDPOINT + '?' + page
+        
+        return self.client.call_api(next_page_endpoint, 'POST', headers=self.headers, data=body_data)
+
     def get_token(self):
         auth_data = dict()
         response_dict= dict()
         auth_data['id'] = self.app_id
         auth_data['secret'] = self.secret_key
-        # auth_data = {'id' : self.app_id, 'secret' : self.secret_key}
+
         try:
             response = self.client.call_api(self.AUTH_ENDPOINT, 'POST', headers=self.headers, data=auth_data)
             
             response_dict['code'] = response.code
-            response_text = json.loads(response.response.text)
+            response_text = json.loads(response.read())
             if response.code == 200:
                 response_dict['token'] = response_text['token']
+                self.token_expiresat = response_text['expiresAt']
             else:
-                response_dict['message'] = response_text
+                response_dict['message'] = 'Authentication Error: Token Generation Failed. ' + response_text['message']
         except Exception as ex:
             if ex.__class__.__name__ is 'ConnectionError':
-                raise ConnectionError(ex)
+                raise ConnectionError('Token Generation Failed: ' + str(ex))
             else:
-                response_dict['type'] = ex.__class__.__name__
-                response_dict['message'] = ex
+                raise ex
 
         return response_dict
+
+    def token_expired(self) -> bool:
+        """Check if the token is expired.
+        :return: True if token is expired, False if not expired
+        :rtype: bool
+        """
+        expires_at = datetime.fromtimestamp(self.token_expiresat)
+        return expires_at >= datetime.now()
--- stix_shifter_modules/reaqta/stix_transmission/connector.py
+++ stix_shifter_modules/reaqta/stix_transmission/connector.py
@@ -6,39 +6,58 @@ from stix_shifter_utils.utils import logger
 
 
 class Connector(BaseSyncConnector):
+    # LOOKS LIKE MAX COUNT is 500. response doesn't show why it fails
+    MAX_LIMIT = 500
+    
     def __init__(self, connection, configuration):
         self.api_client = APIClient(connection, configuration)
         self.logger = logger.set_logger(__name__)
         self.connector = __name__.split('.')[1]
     
     def ping_connection(self):
+        return_obj = dict()
         response_dict = dict()
         try:
             response = self.api_client.ping_data_source()
             response_code = response['code']
             
-            return_obj = dict()
             if response_code == 200:
                 return_obj['success'] = True
             elif response_code == 401:
                 response_dict['type'] = 'AuthenticationError'
-                response_dict['message'] = 'Invalid App Secret key. API Response: {}'.format(response['message'])
+                response_dict['message'] = 'Invalid App Secret key provided. {}'.format(response['message'])
                 ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
             else:
                 response_dict['type'] = 'AuthenticationError'
-                response_dict['message'] = 'Invalid Authentication: Verify App ID. API Response: {}'.format(response['message'])
+                response_dict['message'] = 'Invalid App ID provided. {}'.format(response['message'])
                 ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
             return return_obj
-        except Exception as err:
-            self.logger.error('error when pinging datasource {}:'.format(err))
-            raise
+        except ConnectionError as ex:
+            self.logger.error('error when pinging datasource {}:'.format(ex))
+            response_dict['type'] = 'ConnectionError'
+            response_dict['message'] = 'Invalid hostname provided. {}'.format(ex)
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
+        except Exception as ex:
+            self.logger.error('error when pinging datasource {}:'.format(ex))
+            response_dict['type'] = 'AuthenticationError'
+            response_dict['message'] = 'Authentication Failure. API Response: {}'.format(ex)
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'], connector=self.connector)
+        
+        return return_obj
     
-    def create_results_connection(self, search_id, offset, length):
+    def create_results_connection(self, search_id, offset, length):  
+        return_obj = dict()
+        length = int(length)
+        offset = int(offset)
+        total_records = offset + length
+
         try:
-            min_range = offset
-            max_range = offset + length
-            # Grab the response, extract the response code, and convert it to readable json
-            response = self.api_client.get_search_results(search_id, min_range, max_range)
+            if total_records <= self.MAX_LIMIT:
+                # Grab the response, extract the response code, and convert it to readable json
+                response = self.api_client.get_search_results(search_id, length)
+            elif total_records > self.MAX_LIMIT:
+                response = self.api_client.get_search_results(search_id, self.MAX_LIMIT)
+            
             response_code = response.code
             response_text = response.read()
             try:
@@ -47,17 +66,33 @@ class Connector(BaseSyncConnector):
                 self.logger.debug(response_text)
                 error = Exception(f'Can not parse response from reaqta. The response is not a valid json: {response_text} : {ex}')
             
-            # # Construct a response object
-            return_obj = dict()
             if response_code == 200:
                 return_obj['success'] = True
                 return_obj['data'] = response_dict['result']
+                while len(return_obj['data']) < total_records:
+                    remainings = total_records - len(return_obj['data'])
+                    try:
+                        next_page_url = response_dict['nextPage']
+                        if next_page_url:
+                            response = self.api_client.page_search(search_id, next_page_url, remainings)
+                            response_code = response.code
+                            response_dict = json.loads(response.read())
+                            if response_code == 200:
+                                return_obj['data'].extend(response_dict['result'])
+                        else:
+                            break
+                    except Exception as ex:
+                        raise ex
+                return_obj['data'] = return_obj['data'][offset:total_records]
+            elif response_code == 422:
+                error_string = 'query_syntax_error: ' + response_dict['message']
+                ErrorResponder.fill_error(return_obj, error_string, ['message'], connector=self.connector)
             else:
-                response_string = response_text.decode()
-                ErrorResponder.fill_error(return_obj, response_string, ['message'], connector=self.connector)
-            return return_obj
+                error_string = 'query_syntax_error: ' + response_dict['message']
+                ErrorResponder.fill_error(return_obj, error_string, ['message'], connector=self.connector)
+            
         except Exception as err:
-            self.logger.error('error when getting search results: {}'.format(err))
-            import traceback
-            self.logger.error(traceback.print_stack())
-            raise
+            self.logger.error('error when getting search results: {}'.format(str(err)))
+            ErrorResponder.fill_error(return_obj, err, ['message'], connector=self.connector)
+        
+        return return_obj
