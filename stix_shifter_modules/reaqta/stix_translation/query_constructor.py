1e2e2fb3dd31078bfada1ee64d6a6ba60953f8ef
Arthur Muradyan <arthur.muradyan@ibm.com>
2022-04-04 17:28:49 -0300

    Reaqta connector fixes for match, like, in and or, unittests

--- stix_shifter_modules/reaqta/stix_translation/query_constructor.py
+++ stix_shifter_modules/reaqta/stix_translation/query_constructor.py
@@ -6,6 +6,7 @@ import logging
 import re
 
 UNIVERSAL_FIELDS = ["filename", "ip", "md5", "path", "sha1", "sha256"]
+GREATER_LESS_FIELDS = ["eventdata.regionSize", "eventdata.relevance", "eventdata.size"]
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,8 @@ class QueryStringPatternTranslator:
         self.dmm = data_model_mapper
         self.comparator_lookup = self.dmm.map_comparator()
         self.pattern = pattern
+        self.is_combined_expression = False
+        self.formated_qualifier = None
         self.translated = self.parse_expression(pattern)
 
     @staticmethod
@@ -36,20 +39,22 @@ class QueryStringPatternTranslator:
 
     @staticmethod
     def _negate_comparison(comparison_string):
-        return "NOT ({})".format(comparison_string)
+        """
+        :param comparison_string: str
+        :return: str
+        """
+        if ' OR ' in comparison_string:
+            con_string = re.sub(r'\(', '(NOT ', comparison_string, 1)
+            comparison_string = con_string.replace(' OR ', ' OR NOT ')
+        else:
+            comparison_string = "NOT " + comparison_string
+        return comparison_string
 
     @staticmethod
     def _format_universal_field(field) -> str:
         if field in UNIVERSAL_FIELDS:
             return '${}'.format(field)
         return field
-
-    @staticmethod
-    def _format_qualifier(qualifier) -> str:
-        if isinstance(qualifier, StartStopQualifier):
-            time_interval = 'AND happenedAfter = "' + qualifier.start_iso + '" AND happenedBefore = "' + qualifier.stop_iso + '"'
-            return time_interval
-        return ''
 
     @staticmethod
     def _check_value_type(value):
@@ -60,21 +65,6 @@ class QueryStringPatternTranslator:
         return None
 
     @staticmethod
-    def _parse_mapped_fields(self, expression, value, comparator, stix_field, mapped_fields_array):
-        comparison_string = ""
-        is_reference_value = self._is_reference_value(stix_field)
-        # Need to use expression.value to match against regex since the passed-in value has already been formated.
-        mapped_fields_count = 1 if is_reference_value else len(mapped_fields_array)
-
-        for mapped_field in mapped_fields_array:
-            mapped_field = self._format_universal_field(mapped_field)
-            comparison_string += "{mapped_field} {comparator} {value}".format(mapped_field=mapped_field, comparator=comparator, value=value)
-            if (mapped_fields_count > 1):
-                comparison_string += " OR "
-                mapped_fields_count -= 1
-        return comparison_string
-
-    @staticmethod
     def _is_reference_value(stix_field):
         return stix_field == 'src_ref.value' or stix_field == 'dst_ref.value'
 
@@ -83,6 +73,36 @@ class QueryStringPatternTranslator:
         if str(expression_operator) not in self.comparator_lookup:
             raise NotImplementedError("Comparison operator {} unsupported for connector".format(expression_operator.name))
         return self.comparator_lookup[str(expression_operator)]
+
+    def _set_is_combined_expression(self):
+        self.is_combined_expression = True
+
+    def _format_qualifier(self, qualifier) -> str:
+        if qualifier and isinstance(qualifier, StartStopQualifier):
+            self.formated_qualifier = 'AND happenedAfter = "' + qualifier.start_iso + '" AND happenedBefore = "' + qualifier.stop_iso + '"'
+
+    def _parse_mapped_fields(self, value, comparator, mapped_fields_array) -> str:
+        """Convert a list of mapped fields into a query string."""
+        comparison_strings = []
+        str_ = None
+
+        if isinstance(value, str):
+            value = [value]
+
+        for val in value:
+            for mapped_field in mapped_fields_array:
+                mapped_field = self._format_universal_field(mapped_field)
+                comparison_strings.append(f'{mapped_field} {comparator} {val}')
+
+        # Only wrap in () if there's more than one comparison string
+        if len(comparison_strings) == 1:
+            str_ = comparison_strings[0]
+        elif len(comparison_strings) > 1:
+            str_ = f"{' OR '.join(comparison_strings)}"
+        else:
+            raise RuntimeError((f'Failed to convert {mapped_fields_array} mapped fields into query string'))
+
+        return str_
 
     def _parse_expression(self, expression, qualifier=None) -> str:
         if isinstance(expression, ComparisonExpression):  # Base Case
@@ -96,26 +116,39 @@ class QueryStringPatternTranslator:
             # Some values are formatted differently based on how they're being compared
             if expression.comparator == ComparisonComparators.In:
                 value = self._format_set(expression.value)
+                value = list(map(self._format_equality, expression.value.element_iterator()))
+            elif expression.comparator == ComparisonComparators.Matches:
+                value = self._format_equality(expression.value)
+            elif expression.comparator == ComparisonComparators.Like:
+                value = self._format_equality(expression.value)
             elif expression.comparator == ComparisonComparators.Equal or expression.comparator == ComparisonComparators.NotEqual:
                 # Should be in single-quotes
                 value = self._format_equality(expression.value)
             else:
                 value = self._escape_value(expression.value)
 
-            comparison_string = self._parse_mapped_fields(self, expression, value, comparator, stix_field, mapped_fields_array)
-            if(len(mapped_fields_array) > 1 and not self._is_reference_value(stix_field)):
+            comparison_string = self._parse_mapped_fields(
+                value=value,
+                comparator=comparator,
+                mapped_fields_array=mapped_fields_array
+            )
+
+            mapped_fields_array_len = len(mapped_fields_array)
+
+            if(mapped_fields_array_len > 1):
                 # More than one data source field maps to the STIX attribute, so group comparisons together.
                 grouped_comparison_string = "(" + comparison_string + ")"
                 comparison_string = grouped_comparison_string
 
             if expression.negated:
                 comparison_string = self._negate_comparison(comparison_string)
-            if qualifier is not None:
-                return "{} {}".format(comparison_string, self._format_qualifier(qualifier))
-            else:
-                return "{}".format(comparison_string)
+            
+            self._format_qualifier(qualifier)
+
+            return "{}".format(comparison_string)
 
         elif isinstance(expression, CombinedComparisonExpression):
+            self._set_is_combined_expression()
             operator = self._lookup_comparison_operator(self, expression.operator)
             expression_01 = self._parse_expression(expression.expr1)
             expression_02 = self._parse_expression(expression.expr2)
@@ -125,15 +158,17 @@ class QueryStringPatternTranslator:
                 expression_01 = "({})".format(expression_01)
             if isinstance(expression.expr2, CombinedComparisonExpression):
                 expression_02 = "({})".format(expression_02)
+
             query_string = "{} {} {}".format(expression_01, operator, expression_02)
-            if qualifier is not None:
-                return "{} {}".format(query_string, self._format_qualifier(qualifier))
-            else:
-                return "{}".format(query_string)
+            
+            self._format_qualifier(qualifier)
+            
+            return "{}".format(query_string)
         elif isinstance(expression, ObservationExpression):
             return self._parse_expression(expression.comparison_expression, qualifier)
         elif hasattr(expression, 'qualifier') and hasattr(expression, 'observation_expression'):
             if isinstance(expression.observation_expression, CombinedObservationExpression):
+                self._set_is_combined_expression()
                 operator = self._lookup_comparison_operator(self, expression.observation_expression.operator)
                 expression_01 = self._parse_expression(expression.observation_expression.expr1)
                 # qualifier only needs to be passed into the parse expression once since it will be the same for both expressions
@@ -142,6 +177,7 @@ class QueryStringPatternTranslator:
             else:
                 return self._parse_expression(expression.observation_expression.comparison_expression, expression)
         elif isinstance(expression, CombinedObservationExpression):
+            self._set_is_combined_expression()
             operator = self._lookup_comparison_operator(self, expression.operator)
             expression_01 = self._parse_expression(expression.expr1)
             expression_02 = self._parse_expression(expression.expr2)
@@ -164,5 +200,13 @@ class QueryStringPatternTranslator:
 
 
 def translate_pattern(pattern: Pattern, data_model_mapping, options):
-    query = QueryStringPatternTranslator(pattern, data_model_mapping).translated
+    query_translator = QueryStringPatternTranslator(pattern, data_model_mapping)
+    query = query_translator.translated
+
+    if query_translator.formated_qualifier:
+        if query_translator.is_combined_expression:
+            query = "({}) {}".format(query, query_translator.formated_qualifier)
+        else:
+            query = "{} {}".format(query, query_translator.formated_qualifier)
+
     return query
--- stix_shifter_modules/reaqta/test/stix_translation/test_stix_to_hunt_query.py
+++ stix_shifter_modules/reaqta/test/stix_translation/test_stix_to_hunt_query.py
@@ -15,6 +15,47 @@ class TestQueryTranslator(unittest.TestCase):
 
         self.assertEqual(query, test_string)
 
+    def test_not_operator(self):
+        stix_pattern = "[ipv4-addr:value NOT = '172.31.60.104' OR network-traffic:src_ref.value != '172.31.60.104']" \
+                        "START t'2022-03-24T20:21:35.519Z' STOP t'2022-03-24T20:21:35.619Z'"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['(eventdata.localIp != "172.31.60.104" OR NOT $ip = "172.31.60.104")' \
+                        ' AND happenedAfter = "2022-03-24T20:21:35.519Z" AND happenedBefore = "2022-03-24T20:21:35.619Z"']
+
+        self.assertEqual(query, test_string)
+
+        
+    def test_in_operator(self):
+        stix_pattern = "[network-traffic:src_port IN (443, 446)] OR [ipv4-addr:value IN ('127.0.0.1', '127.0.0.2')] START t'2022-03-24T20:21:35.519Z' STOP t'2022-03-24T20:21:35.619Z'"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['((eventdata.localPort = "443" OR eventdata.localPort = "446") OR ($ip = "127.0.0.1" OR $ip = "127.0.0.2")) AND happenedAfter = "2022-03-24T20:21:35.519Z" AND happenedBefore = "2022-03-24T20:21:35.619Z"']
+
+        self.assertEqual(query, test_string)
+
+        
+    def test_match_operator(self):
+        stix_pattern = "[file:name MATCHES 'serv']"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['$filename = "serv"']
+
+        self.assertEqual(query, test_string)
+
+        
+    def test_like_operator(self):
+        stix_pattern = "[file:name LIKE 'svc']"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['$filename = "svc"']
+
+        self.assertEqual(query, test_string)
+
     def test_ipv4_addr(self):
         stix_pattern = "[ipv4-addr:value = '192.168.122.83' OR ipv4-addr:value = '192.168.122.84']"
         queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
@@ -24,21 +65,48 @@ class TestQueryTranslator(unittest.TestCase):
 
         self.assertEqual(query, test_string)
 
-    def test_network_traffic_src_port(self):
-        stix_pattern = "[network-traffic:src_port = 443] START t'2022-03-24T20:21:35.519Z' STOP t'2022-03-24T20:21:35.619Z'"
+    def test_ipv6_addr(self):
+        stix_pattern = "[ipv6-addr:value = '2001:db8:3333:4444:5555:6666:7777:8888']"
         queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
         query = queries['queries']
 
-        test_string = ['eventdata.localPort = "443" AND happenedAfter = "2022-03-24T20:21:35.519Z" AND happenedBefore = "2022-03-24T20:21:35.619Z"']
+        test_string = ['$ip = "2001:db8:3333:4444:5555:6666:7777:8888"']
+
+        self.assertEqual(query, test_string)
+
+    def test_url(self):
+        stix_pattern = "[url:value = 'https://example.com/example/path']"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['eventdata.url = "https://example.com/example/path"']
 
         self.assertEqual(query, test_string)
 
     def test_file_name(self):
-        stix_pattern = "[file:name = 'file_name']"
+        stix_pattern = "[file:name = 'winword.exe']"
         queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
         query = queries['queries']
 
-        test_string = ['$filename = "file_name"']
+        test_string = ['$filename = "winword.exe"']
+
+        self.assertEqual(query, test_string)
+
+    def test_file_sha1(self):
+        stix_pattern = "[file:hashes.'SHA-1' = 'D56C753E0F8CE84BA3D3AB284628CF6594FDAA74']"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['$sha1 = "D56C753E0F8CE84BA3D3AB284628CF6594FDAA74"']
+
+        self.assertEqual(query, test_string)
+
+    def test_file_sha256(self):
+        stix_pattern = "[file:hashes.'SHA-256' = '47D1D8273710FD6F6A5995FAC1A0983FE0E8828C288E35E80450DDC5C4412DEF']"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['$sha256 = "47D1D8273710FD6F6A5995FAC1A0983FE0E8828C288E35E80450DDC5C4412DEF"']
 
         self.assertEqual(query, test_string)
 
@@ -51,15 +119,50 @@ class TestQueryTranslator(unittest.TestCase):
 
         self.assertEqual(query, test_string)
 
-    def test_combined(self):
-        stix_pattern = "([network-traffic:src_ref.value = '127.0.0.1' AND file:hashes.'MD5' = '23db6982caef9e9152f1a5b2589e6ca3' OR file:hashes.'SHA-256' " \
-                "= 'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad'] AND [ipv4-addr:value = '10.0.0.1' OR ipv4-addr:value = '12.0.0.1' OR " \
-                "ipv4-addr:value = '12.0.0.2'] AND [url:value = 'http://aaa.bbb' OR url:value = 'http://ccc.ddd']) START t'2022-03-30T18:14:52Z' STOP t'2022-03-30T18:19:52Z'"
+    def test_path(self):
+        stix_pattern = "[file:parent_directory_ref.path = 'c:\\\program files\\\microsoft office\\\\root\\\office16\\\winword.exe' OR directory:path = 'c:\\\program files\\\microsoft office\\\\root\\\office16\\\winword.exe']"
         queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
         query = queries['queries']
 
-        test_string = ['($sha256 = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad" OR ($md5 = "23db6982caef9e9152f1a5b2589e6ca3" ' \
-                'AND eventdata.localIp = "127.0.0.1")) AND ($ip = "12.0.0.2" OR ($ip = "12.0.0.1" OR $ip = "10.0.0.1")) AND eventdata.url = "http://ccc.ddd" ' \
-                'OR eventdata.url = "http://aaa.bbb" AND happenedAfter = "2022-03-30T18:14:52Z" AND happenedBefore = "2022-03-30T18:19:52Z"']
+        test_string = ['$path = "c:\\program files\\microsoft office\\root\\office16\\winword.exe" OR $path = "c:\\program files\\microsoft office\\root\\office16\\winword.exe"']
+
+        self.assertEqual(query, test_string)
+
+    def test_network_traffic_src_port(self):
+        stix_pattern = "[network-traffic:src_port = 443] START t'2022-03-24T20:21:35.519Z' STOP t'2022-03-24T20:21:35.619Z'"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['eventdata.localPort = "443" AND happenedAfter = "2022-03-24T20:21:35.519Z" AND happenedBefore = "2022-03-24T20:21:35.619Z"']
+
+        self.assertEqual(query, test_string)
+        
+    def test_network_traffic_ip_port(self):
+        stix_pattern = "[network-traffic:src_ref.value = '169.62.55.114' AND network-traffic:src_port = 3389 AND network-traffic:dst_ref.value = '143.244.41.203' AND network-traffic:dst_port = 60008]"
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['eventdata.remotePort = "60008" AND (eventdata.remoteIp = "143.244.41.203" AND (eventdata.localPort = "3389" AND eventdata.localIp = "169.62.55.114"))']
+
+        self.assertEqual(query, test_string)
+
+
+        
+
+    def test_combined(self):
+        stix_pattern = "([network-traffic:src_ref.value = '127.0.0.1' AND file:hashes.'MD5' != '23db6982caef9e9152f1a5b2589e6ca3' OR file:hashes.'SHA-256'= 'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad']  " \
+                "AND [ipv4-addr:value = '10.0.0.1' OR ipv4-addr:value = '12.0.0.1' OR ipv4-addr:value = '12.0.0.2'] " \
+                "AND [url:value = 'http://aaa.bbb' OR url:value = 'http://ccc.ddd']) START t'2022-03-30T18:14:52Z' STOP t'2022-03-30T18:19:52Z'"
+
+        queries = translation.translate('reaqta', 'query', '{}', stix_pattern)
+        query = queries['queries']
+
+        test_string = ['(' \
+                        '($sha256 = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad" OR ($md5 != "23db6982caef9e9152f1a5b2589e6ca3" AND eventdata.localIp = "127.0.0.1")) ' \
+                        'AND ' \
+                        '($ip = "12.0.0.2" OR ($ip = "12.0.0.1" OR $ip = "10.0.0.1")) ' \
+                        'AND eventdata.url = "http://ccc.ddd" OR eventdata.url = "http://aaa.bbb"' \
+                    ') ' \
+                    'AND happenedAfter = "2022-03-30T18:14:52Z" AND happenedBefore = "2022-03-30T18:19:52Z"']
 
         self.assertEqual(query, test_string)
