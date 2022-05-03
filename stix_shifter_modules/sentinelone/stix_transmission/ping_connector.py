3c871ffa7c9e1e91b006c75ff9ce761b48f0c1bd
lakshmi51974368 <92726459+lakshmi51974368@users.noreply.github.com>
2022-05-04 01:47:33 +0530

    Adding SentinelOne UDI connector (#888)

---
+++ stix_shifter_modules/sentinelone/stix_transmission/ping_connector.py
@@ -0,0 +1,61 @@
+import json
+import time
+from stix_shifter_utils.modules.base.stix_transmission.base_ping_connector \
+    import BasePingConnector
+from stix_shifter_utils.utils.error_response import ErrorResponder
+from requests.exceptions import ConnectionError
+from stix_shifter_utils.utils import logger
+
+
+class PingConnector(BasePingConnector):
+    """Ping connector class """
+    def __init__(self, api_client):
+        self.api_client = api_client
+        self.logger = logger.set_logger(__name__)
+
+    def ping_connection(self):
+        """
+        Ping the endpoint
+        :return: dict
+        """
+        try:
+            # Construct a response object
+            return_obj = {}
+            response_dict = {}
+            return_obj, response_dict = self.call_ping_datasource(return_obj,response_dict)
+        except ConnectionError:
+            response_dict['type'] = "ConnectionError"
+            response_dict['message'] = "Invalid Host"
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        except Exception as ex:
+            if 'Max retries exceeded' in str(ex):
+                # sleep added due to limitation of 1 call a second for each user token
+                try:
+                    time.sleep(1)
+                    return_obj, response_dict = self.call_ping_datasource(return_obj, response_dict)
+                except ConnectionError:
+                    response_dict['type'] = "ConnectionError"
+                    response_dict['message'] = "Invalid Host"
+                    ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+                except Exception as err:
+                    self.logger.error('error when ping: %s', str(err))
+                    response_dict['message'] = str(err)
+                    ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+                return return_obj
+            else:
+                self.logger.error('error when ping: %s', str(ex))
+                ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        return return_obj
+
+    def call_ping_datasource(self, return_obj, response_dict):
+        response = self.api_client.ping_datasource()
+        response_code = response.code
+        response_txt = response.read().decode('utf-8')
+        response_dict = json.loads(response_txt)
+        if response_code == 200:
+            return_obj['success'] = True
+            return_obj['code'] = response_code
+        else:
+            return_obj['success'] = False
+            return_obj['code'] = response_code
+        return return_obj, response_dict
---
+++ stix_shifter_modules/sentinelone/stix_transmission/query_connector.py
@@ -0,0 +1,76 @@
+import json
+from stix_shifter_utils.modules.base.stix_transmission.base_query_connector \
+    import BaseQueryConnector
+from stix_shifter_utils.utils.error_response import ErrorResponder
+from stix_shifter_utils.utils import logger
+from requests.exceptions import ConnectionError
+
+class BadRequestQueryError(Exception):
+    pass
+
+class LimitOutOfRangeError(Exception):
+    pass
+
+class QueryConnector(BaseQueryConnector):
+    """ Query connector base class """
+    def __init__(self, api_client):
+        self.api_client = api_client
+        self.logger = logger.set_logger(__name__)
+
+    def create_query_connection(self, query):
+        """
+        init query
+        :param query
+        :return:queryId
+        """
+        try:
+            # Construct a response object
+            return_obj = {}
+            response_dict = {}
+
+            response = self.api_client.create_search(query)
+            if isinstance(response, dict):
+                return response
+            response_code = response.code
+
+            response_txt = response.read().decode('utf-8')
+            response_dict = json.loads(response_txt)
+
+            if response_code == 200:
+                return_obj['success'] = True
+                return_obj['search_id'] = response_dict['data']['queryId']
+            elif response_code == 401:
+                return_obj['success'] = False
+                response_code = response_dict.get("errors")[0].get("code")
+                if response_code == 4010010:
+                    return_obj['error'] = "Authentication failed"
+            elif response_code == 400:
+                return_obj['success'] = False
+                response_code = response_dict.get("errors")[0].get("code")
+                if response_code == 4000040:
+                    raise BadRequestQueryError
+
+                if response_code == 4000010:
+                    raise LimitOutOfRangeError
+            else:
+                ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+
+        except ConnectionError:
+            response_dict['type'] = "ConnectionError"
+            response_dict['message'] = "Invalid Host"
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        except LimitOutOfRangeError:
+            response_dict['type'] = "LimitOutOfRangeError"
+            response_dict['message'] = "Limit must be greater than or equal to 1 " \
+                                       "and less than or equal to 100000"
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        except BadRequestQueryError:
+            response_dict['type'] = "BadRequestQueryError"
+            response_dict['message'] = response_dict.get("errors")[0].get("detail")
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        except Exception as ex:
+            response_dict['type'] = "unknown"
+            response_dict['message'] = ex
+            self.logger.error('error when creating search: %s', str(ex))
+            ErrorResponder.fill_error(return_obj, response_dict, ['message'])
+        return return_obj
