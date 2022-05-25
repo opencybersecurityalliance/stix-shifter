e8469eee4321f9165c4c70a4d4105b73862622ed
deepakjoshi01234 <deepak.joshi@gslab.com>
2022-05-25 12:12:25 +0530

    changed Hashes with process_hash and removed hardcoded temp data for file_hash

--- stix_shifter_modules/splunk/stix_transmission/results_connector.py
+++ stix_shifter_modules/splunk/stix_transmission/results_connector.py
@@ -26,12 +26,6 @@ class ResultsConnector(BaseResultsConnector):
             return_obj['success'] = True
             return_obj['data'] = results
 
-            # spliting hashes string into SHA256,MD5 and OTHERS
-            for index, val in enumerate(return_obj['data']):
-                has_dict_array = []
-                if 'Hashes' in val:
-                        return_obj['data'][index]['process_hash'] = return_obj['data'][index]['Hashes']
-                        return_obj['data'][index]['file_hash'] = "f5568ea42e4cbd4bcf1f3bf6892d0049"
         else:
             ErrorResponder.fill_error(return_obj, response_dict, ['messages', 0, 'text'], connector=self.connector)
         return return_obj
