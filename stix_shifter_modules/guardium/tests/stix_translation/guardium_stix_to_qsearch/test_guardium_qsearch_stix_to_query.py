f9b6407fd5f2a9c04c972a33fd667eb492350faa
Deepshikha <deepshikha@Deepshikhas-MacBook-Pro.local>
2022-07-04 12:55:04 +0530

    update tests case

--- stix_shifter_modules/guardium/tests/stix_translation/guardium_stix_to_qsearch/test_guardium_qsearch_stix_to_query.py
+++ stix_shifter_modules/guardium/tests/stix_translation/guardium_stix_to_qsearch/test_guardium_qsearch_stix_to_query.py
@@ -109,15 +109,15 @@ class TestQueryTranslator(unittest.TestCase, object):
         stix_pattern = "[ipv4-addr:value IN ('127.0.0.1', '127.0.0.2')]"
         query = translation.translate('guardium', 'query', '{}', stix_pattern)
         filters = "\"ServerIP\": \"127.0.0.1\""
-        _test_query_assertions(query['queries'], 1, filters)
+        _test_query_assertions(query['queries'], 0, filters)
         filters = "\"ServerIP\": \"127.0.0.2\""
-        _test_query_assertions(query['queries'], 1, filters)
-        filters = "\"filters\":\"name=Client IP&value=127.0.0.1&isGroup=false\""
-        _test_query_assertions(query['queries'], 1, filters)
+        _test_query_assertions(query['queries'], 0, filters)
+        filters = "\"filters\":\"name=ClientIP&value=127.0.0.1&isGroup=false\""
+        _test_query_assertions(query['queries'], 0, filters)
         filters = "\"filters\":\"name=Server&value=127.0.0.2&isGroup=false\""
-        _test_query_assertions(query['queries'], 1, filters)
+        _test_query_assertions(query['queries'], 0, filters)
         filters = "\"filters\":\"name=Server&value=127.0.0.1&isGroup=false\""
-        _test_query_assertions(query['queries'], 1, filters)
-        filters = "\"filters\":\"name=Client IP&value=127.0.0.2&isGroup=false\""
-        _test_query_assertions(query['queries'], 1, filters)
+        _test_query_assertions(query['queries'], 0, filters)
+        filters = "\"filters\":\"name=ClientIP&value=127.0.0.2&isGroup=false\""
+        _test_query_assertions(query['queries'], 0, filters)
 
