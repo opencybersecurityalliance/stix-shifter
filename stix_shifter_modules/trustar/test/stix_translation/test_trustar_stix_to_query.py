485a317648d8d34c83e15349507b8100c3e5079f
deepsiIBM <98740866+deepsiIBM@users.noreply.github.com>
2022-07-15 15:41:42 +0530

    Update test_trustar_stix_to_query.py

--- stix_shifter_modules/trustar/test/stix_translation/test_trustar_stix_to_query.py
+++ stix_shifter_modules/trustar/test/stix_translation/test_trustar_stix_to_query.py
@@ -12,6 +12,7 @@ DEFAULT_TIMERANGE = 5
 
 translation = stix_translation.StixTranslation()
 
+
 def _set_start_stop(query):
     stop_time = datetime.datetime.utcnow()
     go_back_in_minutes = datetime.timedaelta(minutes=DEFAULT_TIMERANGE)
@@ -21,6 +22,12 @@ def _set_start_stop(query):
     converted_starttime = start_time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
     converted_stoptime = stop_time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
     return converted_starttime, converted_stoptime
+
+def _test_query_assertions(query,selections, from_statement, where_statement):
+    assert query['queries'] == [selections + from_statement + where_statement]
+
+def _translate_query(stix_pattern):
+    return translation.translate('trustar', 'query', '{}', stix_pattern)
 
 
 class TestStixtoQuery(unittest.TestCase, object):
@@ -35,25 +42,27 @@ class TestStixtoQuery(unittest.TestCase, object):
         self._test_query_assertions(translated_query, test_query)
 
     def test_searchterm(self):
-        stix_pattern = "[x-trustar-query:x_searchTerm = 'redhat@gmail.com'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
+        stix_pattern = "[x-trustar-report:searchTerm = 'redhat@gmail.com'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
         translated_query = translation.translate('trustar', 'query', '{}', stix_pattern)
         test_query = ["{\"searchTerm\": \"redhat@gmail.com\", \"from\": 1630155241000, \"to\": 1630846441000}"]
         self._test_query_assertions(translated_query, test_query)
 
     def test_entitytype(self):
-        stix_pattern = "[x-trustar-query:x_type = 'IP, MALWARE'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
+        stix_pattern = "[x-trustar-report:type = 'IP, MALWARE'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
         translated_query = translation.translate('trustar', 'query', '{}', stix_pattern)
         test_query = ["{\"entityTypes\": \"IP, MALWARE\", \"from\": 1630155241000, \"to\": 1630846441000}"]
         self._test_query_assertions(translated_query, test_query)
 
     def test_ipv4(self):
         stix_pattern = "[ipv4-addr:value = '127.0.0.1'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
-        translated_query = translation.translate('trustar', 'query', '{}', stix_pattern)
+        # stix_pattern = "[ipv4-addr:value = '192.168.122.83']"
+        query = _translate_query(stix_pattern)
+        # translated_query = translation.translate('trustar', 'query', '{}', stix_pattern)
         test_query = ["{\"searchTerm\": \"127.0.0.1\", \"from\": 1630155241000, \"to\": 1630846441000}"]
-        self._test_query_assertions(translated_query, test_query)
+        self._test_query_assertions(query, test_query)
 
     def test_report(self):
-        stix_pattern = "[x-trustar-query:x_report_details = '9e509316-b8f2-428e-9981-0e552bb128bf'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
+        stix_pattern = "[x-trustar-report:report_details  = '9e509316-b8f2-428e-9981-0e552bb128bf'] START t'2021-08-28T12:54:01.009Z' STOP t'2021-09-05T12:54:01.009Z'"
         translated_query = translation.translate('trustar', 'query', '{}', stix_pattern)
         test_query = ["{\"reportId\": \"9e509316-b8f2-428e-9981-0e552bb128bf\", \"from\": 1630155241000, \"to\": 1630846441000}"]
         self._test_query_assertions(translated_query, test_query)
