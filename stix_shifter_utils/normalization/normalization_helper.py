73c86b090ef047fb8651409eddf90239451c2fcb
SalmanMesia <salmanmesia@gmail.com>
2022-08-15 13:43:28 -0400

    Updated requirements and helper to use python library

--- stix_shifter_utils/normalization/normalization_helper.py
+++ stix_shifter_utils/normalization/normalization_helper.py
@@ -1,5 +1,5 @@
 from ipaddress import ip_network, IPv4Network, IPv6Network
-import validators
+from urllib.parse import urlparse
 
 def create_attributes(attribute_fields, data):
   ''' Used to create a custom property of type array of objects to add on a STIX 2.1 SDO
@@ -53,7 +53,7 @@ def evaluate_attribute_type(attribute):
     attribute_type = 'number'
   if isinstance(attribute, (str)):
     attribute_type = 'string'
-    if validators.url(attribute):
+    if uri_validator(attribute):
       attribute_type = 'uri'
     try:
       if isinstance(ip_network(attribute), (IPv4Network, IPv6Network)):
@@ -62,3 +62,9 @@ def evaluate_attribute_type(attribute):
       pass
 
   return attribute_type
+
+
+def uri_validator(x):
+  # Check if string is a URI string
+  result = urlparse(x)
+  return all([result.scheme, result.netloc])
