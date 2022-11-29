cdb174eb5a89b87a2fa75a6780f7034fc548902c
Danny Elliott <danny.elliott@ibm.com>
2022-11-29 11:04:24 -0400

    test timestamps with various milliseconds

--- tests/stix_translation/test_start_stop_qualifier.py
+++ tests/stix_translation/test_start_stop_qualifier.py
@@ -2,24 +2,26 @@ from stix_shifter.stix_translation import stix_translation
 from stix_shifter_utils.utils.module_discovery import modules_list
 
 translation = stix_translation.StixTranslation()
-STIX_PATTERN = "[ipv4-addr:value = '192.168.0.100'] START t'2022-11-18T15:00:00Z' STOP t'2022-11-20T16:00:00Z'"
+START_STOP_TIMESTAMPS = {
+    "start_stop_no_milliseconds": "START t'2022-11-18T15:00:00Z' STOP t'2022-11-20T16:00:00Z'",
+    "start_stop_partial_milliseconds": "START t'2022-11-18T15:00:00.1Z' STOP t'2022-11-20T16:00:00.2Z'",
+    "start_stop_full_milliseconds": "START t'2022-11-18T15:00:00.100Z' STOP t'2022-11-20T16:00:00.200Z'"
+}
+STIX_PATTERN = "[ipv4-addr:value = '192.168.0.100']"
 
 
 class TestQualifierTimestamps(object):
-    translation_errors ={}
 
-    def validate_stix_result(self, result, module):
+    def _validate_stix_result(self, result, module, timestamp):
         if 'error' in result:
             if "timestamp" not in result["error"]:
                 next
             else:
-                self.translation_errors[module] = result['error']
+                assert False, "{} failed to translate timestamp: {}".format(module, timestamp)
 
     def test_supported_dialects(self):
-        
         modules = modules_list()
         for module in modules:
-            result = translation.translate(module, 'query', '', STIX_PATTERN, {})
-            self.validate_stix_result(result, module)
-        if self.translation_errors:
-            assert False, "The following modules failed STIX translation {}".format(self.translation_errors) 
+            for key, timestamp in START_STOP_TIMESTAMPS.items():
+                result = translation.translate(module, 'query', '', STIX_PATTERN + " " + timestamp)
+                self._validate_stix_result(result, module, timestamp)
