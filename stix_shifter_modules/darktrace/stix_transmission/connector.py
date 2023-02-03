43ecec8c1f14a36b8659e13edb34ec30dfbd8834
Danny Elliott <danny.elliott@ibm.com>
2023-02-03 11:18:29 -0400

    remove unnecessary variable definition

--- stix_shifter_modules/darktrace/stix_transmission/connector.py
+++ stix_shifter_modules/darktrace/stix_transmission/connector.py
@@ -82,8 +82,6 @@ class Connector(BaseJsonSyncConnector):
 
             response_dict = json.loads(response_str)
 
-            response_dict = json.loads(response_wrapper.read().decode('utf-8'))
-
             if response_dict.get('error'):
                 raise InvalidArguments(response_dict['error'])
 
