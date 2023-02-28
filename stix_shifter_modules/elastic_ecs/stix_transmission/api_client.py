1f007316ae8fa428b25263785c75788e0e1ae194
tingdai <danielday1128@gmail.com>
2023-02-28 14:41:56 -0500

    remove depricated run_search() function

--- stix_shifter_modules/elastic_ecs/stix_transmission/api_client.py
+++ stix_shifter_modules/elastic_ecs/stix_transmission/api_client.py
@@ -56,60 +56,6 @@ class APIClient():
     def ping_box(self):
         return self.client.call_api(self.PING_ENDPOINT, 'GET', timeout=self.timeout)
 
-    def run_search(self, query_expression, offset=None, length=DEFAULT_LIMIT):
-        headers = dict()
-        headers['Content-Type'] = 'application/json'
-
-        endpoint = self.endpoint
-
-        uri_search = False  # For testing and debugging two ways of _search API methods
-
-        # URI Search
-        if uri_search:
-            if query_expression is not None:
-                # update/add size value
-                if length is not None:
-                    if re.search(r"&size=\d+", query_expression):
-                        query_expression = re.sub(r"(?<=&size=)\d+", str(length), query_expression)
-                    else:
-                        query_expression = '{}&size={}'.format(query_expression, length)
-
-                # add offset to query expression
-                if offset is not None:
-                    query_expression = '{}&from={}'.format(query_expression, offset)
-
-            # addition of QueryString to API END point
-            endpoint = endpoint + '?q=' + query_expression
-
-            return self.client.call_api(endpoint, 'GET', headers, timeout=self.timeout)
-        # Request body search
-        else:
-            # add size value
-            if length is not None:
-                endpoint = "{}?size={}".format(endpoint, length)
-
-            # add offset value
-            if offset is not None:
-                endpoint = "{}&from={}".format(endpoint, offset)
-
-            data = {
-                "_source": {
-                    "includes": ["@timestamp", "source.*", "destination.*", "event.*", "client.*", "server.*",
-                                 "host.*", "network.*", "process.*", "user.*", "file.*", "url.*", "registry.*", "dns.*",
-                                 "tags"]
-                },
-                "query": {
-                    "query_string": {
-                        "query": query_expression
-                    }
-                }
-            }
-
-            self.logger.debug("URL endpoint: " + endpoint)
-            self.logger.debug("URL data: " + json.dumps(data))
-
-            return self.client.call_api(endpoint, 'GET', headers, data=json.dumps(data), timeout=self.timeout)
-
     def search_pagination(self, query_expression, lastsortvalue=None, length=DEFAULT_LIMIT):
         headers = dict()
         headers['Content-Type'] = 'application/json'
