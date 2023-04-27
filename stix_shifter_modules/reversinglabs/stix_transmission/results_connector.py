9f6160f4efced42f6477ec5afe6a3806a7c2e15f
Salman Mesia <salmanmesia@gmail.com>
2023-04-27 13:22:01 -0400

    Update Reversinglabs connector (#1436)

--- stix_shifter_modules/reversinglabs/stix_transmission/results_connector.py
+++ stix_shifter_modules/reversinglabs/stix_transmission/results_connector.py
@@ -15,15 +15,15 @@ class ResultsConnector(BaseJsonResultsConnector):
             # print(search_id, min_range, max_range, length)
             search_id = search_id.replace('\'', "\"")
             query_json= json.loads(search_id)
-            response = await self.api_client.get_search_results(query_json)
-            response_code = response['code']
+            response, response_code = await self.api_client.get_search_results(query_json)
+            print(response)
 
             # Grab the response, extract the response code, and convert it to readable json
             # response_dict = self.api_client.get_search_results(search_id, min_range, max_range)
             # json_data = json.loads(response.read().decode('utf-8'))
             # print(type(response), response)
             # response_code = response.code
-            json_data = response['message']
+            json_data = response
 
             if "rl" in json_data.keys():
                 json_data['rl'] = [json_data['rl']]
@@ -31,6 +31,9 @@ class ResultsConnector(BaseJsonResultsConnector):
                 rl_data = {}
                 rl_data['rl'] = [{'message': 'IOC not found'}]
                 json_data = rl_data
+
+
+
 
             # # Construct a response object
             return_obj = dict()
@@ -46,15 +49,13 @@ class ResultsConnector(BaseJsonResultsConnector):
                 return_obj['success'] = True
                 return_obj['data'] = [json_data]
 
-                ErrorResponder.fill_error(return_obj, response, ['message'], connector='reversinglabs')
-
             # else:
             #     # ErrorResponder.fill_error(return_obj, response_dict, ['message'])
             #     ErrorResponder.fill_error(return_obj, json_data)
             #     return_obj['error'] = json_data['error']
             return return_obj
         except Exception as err:
-            response['message'] = str(err)
             self.logger.error('error when getting search results: {}'.format(err))
-            ErrorResponder.fill_error(return_obj, response, ['message'], connector='reversinglabs')
+            import traceback
+            self.logger.error(traceback.print_stack())
             raise
