9eeee2f73166c533c98ec57d81936e939da04bfd
Md Azam <mdazam@ca.ibm.com>
2023-05-12 11:52:46 -0300

    unittest fix

--- stix_shifter_modules/azure_sentinel/tests/stix_translation/test_azure_sentinel_stix_to_query.py
+++ stix_shifter_modules/azure_sentinel/tests/stix_translation/test_azure_sentinel_stix_to_query.py
@@ -304,7 +304,7 @@ class TestQueryTranslator(unittest.TestCase):
             }
         ]
         queries = _remove_timestamp_from_query(queries)
-        self._test_query_assertions(query, queries)
+        self.assertListEqual(query['queries'], queries)
     
     def test_lamda_operator_with_collection(self):
         stix_pattern = "[x-ibm-finding:x_recommendedActions = 'Enforce' OR x-ibm-finding:x_detectionIds != '111']"
