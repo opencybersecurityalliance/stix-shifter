eb0954c596cf169019b7cd4178703eaef6c4d2e0
Md Azam <mdazam@ca.ibm.com>
2023-07-31 18:36:33 -0300

    update4

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
