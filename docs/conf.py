84a36c08ca203b250f6c16adb5944a061e2290ef
Md Azam <mdazam@ca.ibm.com>
2023-08-15 11:34:10 -0300

    html theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,5 +48,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+html_theme_options = {
+    "home_page_in_toc": True,
+    "github_url": "https://github.com/opencybersecurityalliance/stix-shifter",
+    "repository_url": "https://github.com/opencybersecurityalliance/stix-shifter",
+    "repository_branch": "master",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "announcement": "<b>6.1.0</b> is now out! See the Changelog for details",
+}
