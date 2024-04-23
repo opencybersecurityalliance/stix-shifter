3666240d1af171a4139d9e484fc50b531e314ad2
Xiaokui Shu <subbyte@gmail.com>
2024-04-23 13:28:39 -0400

    fix tests

--- stix_shifter_modules/elastic_ecs/tests/stix_translation/test_elastic_ecs_stix_to_query.py
+++ stix_shifter_modules/elastic_ecs/tests/stix_translation/test_elastic_ecs_stix_to_query.py
@@ -353,14 +353,14 @@ class TestStixtoQuery(unittest.TestCase, object):
         stix_pattern = r"[process:command_line MATCHES 'cmd\\.exe .*']"
         translated_query = translation.translate('elastic_ecs:beats', 'query', '{}', stix_pattern)
         translated_query['queries'] = _remove_timestamp_from_query(translated_query['queries'])
-        test_query = ['(process.command_line.keyword : /cmd\\.exe .*/ OR powershell.command.value : /cmd\\.exe .*/)']
+        test_query = ['(process.command_line.keyword : /cmd\\.exe .*/)']
         _test_query_assertions(translated_query, test_query)
 
     def test_like_operator(self):
         stix_pattern = "[process:command_line LIKE 'cmd.exe /_ %']"
         translated_query = translation.translate('elastic_ecs', 'query', '{}', stix_pattern)
         translated_query['queries'] = _remove_timestamp_from_query(translated_query['queries'])
-        test_query = ['(process.command_line : "cmd.exe /? *" OR powershell.command.value : "cmd.exe /? *")']
+        test_query = ['(process.command_line : "cmd.exe /? *")']
         _test_query_assertions(translated_query, test_query)
 
     def test_like_operator_with_backslash(self):
