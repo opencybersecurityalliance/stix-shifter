65830c0227f5275fcd3565a6462612763baabd56
Omar Daniel <104202872+omarrdaniel@users.noreply.github.com>
2024-05-08 16:37:29 +0100

    Added dialects from cloud siem Sumologic (#1686)

--- stix_shifter_modules/sumologic/entry_point.py
+++ stix_shifter_modules/sumologic/entry_point.py
@@ -1,4 +1,7 @@
+import os
+
 from stix_shifter_utils.utils.base_entry_point import BaseEntryPoint
+from .stix_translation.query_translator import QueryTranslator
 
 
 class EntryPoint(BaseEntryPoint):
@@ -8,5 +11,14 @@ class EntryPoint(BaseEntryPoint):
         if connection:
             self.setup_transmission_simple(connection, configuration)
 
-        self.setup_translation_simple(dialect_default='default')
+        basepath = os.path.dirname(__file__)
+        filepath = os.path.abspath(os.path.join(basepath, "stix_translation"))
+
+        dialect = 'default'
+        query_translator = QueryTranslator(options, dialect, filepath)
+        self.add_dialect(dialect, query_translator=query_translator, default_include=True, default=True)
+
+        dialect = 'cloud_siem'
+        query_translator = QueryTranslator(options, dialect, filepath)
+        self.add_dialect(dialect, query_translator=query_translator, default_include=False, default=False)
 
